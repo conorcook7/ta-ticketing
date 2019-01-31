3dc6df52ee3209026b4ebfed706e8883133cc299
Hayden Phothong <HaydenPhothong@u.boisestate.edu>
2019-01-30 19:39:24 -0700

    Adding index.php to the repository

---
+++ index.php
@@ -0,0 +1,15 @@
+</html>
+	<head>
+		<title>TA Ticketing Service</title>
+	</head>
+	<body>
+		<h1>TA Ticketing Service</h1>
+		<h2>Welcome!</h2>
+		<?php
+			for ($i = 0; $i < 5; $i++) {
+				echo "PHP is working!";
+			}
+		?>
+	</body>
+</html>
+
