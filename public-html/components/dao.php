3d30ad3f1982c9e8b152affa802413d197e5c47b
hphothong <hphothong@live.com>
2019-02-17 14:56:31 -0700

    Adding dao changes

--- public-html/components/dao.php
+++ public-html/components/dao.php
@@ -2,108 +2,186 @@
 
 //require_once 'KLogger.php';
 
+/**
+ * The data access object (Dao) for the website. This object uses a standard user for the local mysql instance.
+ * The user is only created in the Dummy_TA_Ticketing database, it will have to be added manually to the production DB.
+ */
 class Dao {
+  
+    $SUCCESS = TRUE;
+    $FAILURE = FALSE;
 
-  private $db = "Dummy_TA_Ticketing";
-  private $user = "ta-ticketing";
-  private $pass = "34$5iu98&7o7%76d4Ss35";
-  protected $logger;
-  
-  public function __construct () {
-    //$this->logger = new KLogger('../../ta-ticketing.log', KLogger::DEBUG);
-  }
-  
-  public function getConnection () {
-    try{
-        $conn = new PDO("mysql:host=localhost;dbname={$this->db}", $this->user, $this->pass);
-        //$this->logger->logDebug("Established a database connection.");
-        return $conn;
-    } catch (Exception $e) {
-        echo "connection failed: " . $e->getMessage();
-        //$this->logger->logFatal("The database connection failed.");
+    private $db = "Dummy_TA_Ticketing";
+    private $user = "ta-ticketing";
+    private $pass = "34$5iu98&7o7%76d4Ss35";
+
+    protected $logger;
+
+    public function __construct () {
+        //$this->logger = new KLogger('../../ta-ticketing.log', KLogger::DEBUG);
     }
-  }
-
-  private function saltPassword($password) {
-    $salt = '!@%#^^%*&;rweltkjusofd;iajg168152410';
-    return md5($password . $salt);
-  }
-
-  public function userExists($email){
-    $conn = $this->getConnection();
-    $query = $conn->prepare("SELECT COUNT(*) FROM Users where email = :email;");
-    $query->bindParam(':email', $email);
-    $query->execute();
-    $result = $query->fetch(PDO::FETCH_ASSOC);
-    return $result["COUNT(*)"];
-  }
-
-  public function getUsers(){
-    $conn = $this->getConnection();
-    $query = $conn->prepare("SELECT * FROM Users;");
-    $query->setFetchMode(PDO::FETCH_ASSOC);
-    $query->execute();
-    $results = $query->fetchAll();
-    //$this->logger->logDebug(__FUNCTION__ . " " . print_r($results,1));
-    return $results;
-  }
   
-  
-  
-  public function login($username, $password){
-    $salt = '!@%#^^%*&;rweltkjusofd;iajg168152410';
-    $password=md5($password . $salt);
-    $conn = $this->getConnection();
-    $query = $conn->prepare("SELECT * FROM user where username = :username AND password = :password");
-    $query->bindParam(':username', $username);
-    $query->bindParam(':password', $password);
-    $query->execute();
-    $results=$query->fetch(PDO::FETCH_ASSOC);
-    if (is_array($results) && 0 < count($results)){
-        return true;
-    } else {
-        return false;
-    }    
-  }
-  
-  public function getUser($username){
-    $conn = $this->getConnection();
-    $query = $conn->prepare("SELECT * FROM user WHERE username = :username");
-    $query->bindParam(':username', $username);
-    $query->execute();
-    $results = $query->fetch(PDO::FETCH_ASSOC);
-    //$this->logger->logDebug(__FUNCTION__ . " " . print_r($results,1));
-    return $results;
-  }
-  
-  public function saveUser($username, $email, $password){
-    $salt = '!@%#^^%*&;rweltkjusofd;iajg168152410';
-    $password=md5($password . $salt);
-    $conn = $this->getConnection();
-    $query = $conn->prepare("INSERT INTO user (username, email, password, exp, currentGoals, completeGoals) VALUES (:username, :email, :password, 0, 0, 0)");
-    $query->bindParam(':username',$username);
-    $query->bindParam(':email', $email);
-    $query->bindParam(':password', $password);
-    //$this->logger->logDebug(__FUNCTION__ . " username=[{$username}] email=[{$email}]");
-    $query->execute();
-  }
-  
-  public function checkAccess($username) {
-    $conn = $this->getConnection();
-    $query = $conn->prepare("SELECT * FROM user where username = :username AND access = '1'");
-    $query->bindParam(':username', $username);
-    $query->execute();
-    $results=$query->fetch(PDO::FETCH_ASSOC);
-    if (is_array($results) && 0 < count($results)){
-        return true;
-    } else {
-        return false;
+    /**
+     * Attempts to connect to the local MySQL instance with the user ta-ticketing.
+     * @return $conn - The connection to the localhost MySQL database.
+     */
+    public function getConnection () {
+        try{
+            $conn = new PDO("mysql:host=localhost;dbname={$this->db}", $this->user, $this->pass);
+            //$this->logger->logDebug("Established a database connection.");
+            return $conn;
+        } catch (Exception $e) {
+            echo "connection failed: " . $e->getMessage();
+            //$this->logger->logFatal("The database connection failed.");
+        }
     }
-  }
-  
-  public function verifyPassword($password){
-    $regex='/^\S*(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/';
-    return preg_match($regex, $password);
-  }
+
+    /**
+     * Hashes the password with a salt using the MD5 encryption method.
+     * @param $password - The password to hash.
+     * @return Returns the hashed password.
+     */
+    private function hashPassword($password) {
+        $salt = '!@%#^^%*&;rweltkjusofd;iajg168152410';
+        return md5($password . $salt);
+    }
+
+    /**
+     * Checks if a user exists in the database already.
+     * @param $email - The email of the user to check.
+     * @return Returns TRUE if the user exists, else FALSE.
+     */
+    public function userExists($email){
+        $conn = $this->getConnection();
+        $query = $conn->prepare("SELECT COUNT(*) FROM Users where email = :email;");
+        $query->bindParam(':email', $email);
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Returns the user with the specific email address.
+     * @param $email - The email address of the user.
+     * @return $user - The array of user data.
+     */
+    public function getUser($email) {
+        $conn = $this->getConnection();
+        $query = $conn->prepare("SELECT * FROM Users where email = :email;");
+        $query->bindParam(':email', $email);
+        $query->execute();
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+        return $user
+    }
+
+    /**
+     * Checks if the user is a TA.
+     * @param $user_id - The user_id of the active user.
+     */
+    public function isTA($userId) {
+        $conn = $this->getConnection();
+        $query = $conn->prepare("SELECT COUNT(*) FROM Teaching_Assistants where user_id = :userId;");
+        $query->bindParam(":userId", $userId);
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Create a user if they do not exist in the database.
+     * @param $email - The email address of the user to create.
+     * @param $password - The raw password of the user to create.
+     * @param $firstName - The first name of the user if given, else NULL.
+     * @param $lastName - The last name of the user if given, else NULL.
+     * @return Returns TRUE if the user was created, else FALSE
+     */
+    public function createUser($email, $password, $firstName=NULL, $lastName=NULL) {
+        $exists = $this->userExists($email);
+        if (!$exists) {
+            $conn = $this->getConnection();
+            $query = $conn->prepare(
+                "INSERT INTO Users (permission_id, email, password, first_name, last_name) " .
+                "VALUES (1, :email, :hashedPassword, :firstName, :lastName);"
+            );
+            $query->bindParam(":email", $email);
+            $query->bindParam(":hashedPassword", $this->hashPassword($password));
+            $query->bindParam(":firstName", $firstName);
+            $query->bindParam(":lastName", $lastName);
+            $status = $query->execute();
+            if (status) {
+                return $this->$SUCCESS;
+            }
+        }
+        return $this->$FAILURE;
+    }
+
+    /**
+     * Returns all of the users.
+     * @return $users - All of the users from the Users table.
+     */
+    public function getUsers(){
+        $conn = $this->getConnection();
+        $query = $conn->prepare("SELECT * FROM Users;");
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+        $query->execute();
+        $users = $query->fetchAll();
+        //$this->logger->logDebug(__FUNCTION__ . " " . print_r($users,1));
+        return $users;
+    }
+
+    /**
+     * Returns all of the teaching assistants with information about them.
+     * @return $teachingAssistants - The array of arrays for each teaching assistant.
+     */
+    public function getTeachingAssistants() {
+        $conn = $this->getConnection();
+        $query = $conn->prepare("SELECT * FROM Teaching_Assistants JOIN Users ON Teaching_Assistants.user_id = Users.user_id;");
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+        $query->execute();
+        $teachingAssistants = $query->fetchAll();
+        //$this->logger->logDebug(__FUNCTION__ . " " . print_r($teachingAssistants,1));
+        return $teachingAssistants;
+    }
+    
+    public function login($username, $password){
+        $salt = '!@%#^^%*&;rweltkjusofd;iajg168152410';
+        $password=md5($password . $salt);
+        $conn = $this->getConnection();
+        $query = $conn->prepare("SELECT * FROM user where username = :username AND password = :password");
+        $query->bindParam(':username', $username);
+        $query->bindParam(':password', $password);
+        $query->execute();
+        $results=$query->fetch(PDO::FETCH_ASSOC);
+        if (is_array($results) && 0 < count($results)){
+            return true;
+        } else {
+            return false;
+        }    
+    }
+    
+    public function checkAccess($username) {
+        $conn = $this->getConnection();
+        $query = $conn->prepare("SELECT * FROM user where username = :username AND access = '1'");
+        $query->bindParam(':username', $username);
+        $query->execute();
+        $results=$query->fetch(PDO::FETCH_ASSOC);
+        if (is_array($results) && 0 < count($results)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function verifyPassword($password){
+        $regex='/^\S*(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/';
+        return preg_match($regex, $password);
+    }
 }
-
