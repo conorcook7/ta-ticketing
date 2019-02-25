9bad813e6588ea16c1233be011278f0bad84c37c
hphothong <hphothong@live.com>
2019-02-25 14:10:13 -0700

    testing base functionality

--- public-html/auth/google-auth/google-callback.php
+++ public-html/auth/google-auth/google-callback.php
@@ -2,27 +2,35 @@
 
     require_once "config.php";
 
-    if (isset($_SESSION["access_token"])) {
-        $client->setAccessToken($_SESSION["access_token"]);
+    // if (isset($_SESSION["access_token"])) {
+    //     $client->setAccessToken($_SESSION["access_token"]);
 
-    } else if (isset($_GET["code"])) {
+    // } else if (isset($_GET["code"])) {
+    //     $token = $client->fetchAccessTokenWithAuthCode($_GET["code"]);
+    //     $_SESSION["access_token"] = $token;
+
+    // } else {
+    //     header("Location: ../../pages/google-index.php");
+    //     exit();
+    // }
+
+
+
+    if (isset($_GET["code"])) {
         $token = $client->fetchAccessTokenWithAuthCode($_GET["code"]);
         $_SESSION["access_token"] = $token;
-
-    } else {
-        header("Location: ../../pages/google-index.php");
-        exit();
     }
+
 
     // Store user data in the session.
     $google_oauth = new Google_Service_Oauth2($client);
     $userData = $google_oauth->userinfo_v2_me->get();
-    $_SESSION['user']['id'] = $userData['id'];
-    $_SESSION['user']['email'] = $userData['email'];
-    $_SESSION['user']['familyName'] = $userData['familyName'];
-    $_SESSION['user']['givenName'] = $userData['givenName'];
-    $_SESSION['user']['picture'] = $userData['picture'];
-    $_SESSION['user']['gender'] = $userData['gender'];
+    // $_SESSION['user']['id'] = $userData['id'];
+    // $_SESSION['user']['email'] = $userData['email'];
+    // $_SESSION['user']['familyName'] = $userData['familyName'];
+    // $_SESSION['user']['givenName'] = $userData['givenName'];
+    // $_SESSION['user']['picture'] = $userData['picture'];
+    // $_SESSION['user']['gender'] = $userData['gender'];
 
     echo "<pre>" . print_r($userData, 1) . "</pre>"; 
 
