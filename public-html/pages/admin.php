b4a263029ac79577f74c8c14d071053dc43fcbc4
Malik Herring <MalikHerring@u.boisestate.edu>
2019-02-26 07:13:02 -0700

    adding in the beginning of the admin page

---
+++ public-html/pages/admin.php
@@ -0,0 +1,28 @@
+<?php require_once '../components/header.php'; ?>
+
+<body id="page-top">
+
+  <div id="wrapper">
+  
+    <!-- Start of Sidebar -->
+    <?php include_once '../components/sidebar.php'; ?>
+    <!-- End of Sidebar -->
+    
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+
+
+        <!-- Topbar -->
+        <?php include_once '../components/topbar.php'; ?>
+        <!-- End of Topbar -->
+      
+      </div>
+    </div>
+  </div>
+</body>
+
+<?php require_once '../components/footer.php'; ?>
