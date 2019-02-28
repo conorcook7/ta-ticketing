9ede1ac4ca9aaf3516cd39496f01a762b7ebdc0f
Malik Herring <malikherring@u.boisestate.edu>
2019-02-27 17:03:01 -0700

    Changing them back

--- public-html/components/dao.php
+++ public-html/components/dao.php
@@ -8,8 +8,8 @@
  */
 class Dao {
   
-    private $SUCCESS = 1;
-    private $FAILURE = 0;
+    private $SUCCESS = TRUE;
+    private $FAILURE = FALSE;
 
     private $db;
     private $user = "ta-ticketing";
