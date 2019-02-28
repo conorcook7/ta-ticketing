8902752dcb02b6f33408b3dae7b9d4da8d7ecdfb
Malik Herring <MalikHerring@u.boisestate.edu>
2019-02-27 17:06:20 -0700

    added dao usage

--- public-html/pages/admin.php
+++ public-html/pages/admin.php
@@ -1,6 +1,7 @@
 <?php 
   require_once '../components/header.php';
-  require_once '../components/dao.php'; ?>
+  require_once '../components/dao.php'; 
+  $dao = new Dao("Dummy_TA_Ticketing")?>
 
 <body id="page-top">
 
@@ -32,6 +33,18 @@
           </tr>
           </thead>
           <tbody>
+          <?php 
+            $users = $dao->getUsers();
+            $count = 1;
+            foreach($users as $user) { ?>
+            <tr>
+              <th scope="row"><?php echo $count; ?></th>
+              <td><?php echo htmlspecialchars($user['first_name']) . " " . htmlspecialchars($user['last_name']); ?></td>
+              <td><?php echo htmlspecialchars($user['email']); ?></td>
+              <td><?php echo $user['permission_id']; ?></td>
+              <td><?php echo $user['user_id']; ?></td>
+            </tr>
+          <?php $count += 1;} ?>
           </tbody>
         </table>
       </div>
