3f721549b533f4efb702903947109f12fb79f4c9
msanchez563 <michaelsanchez563@u.boisestate.edu>
2019-03-06 20:21:25 -0700

    Added missing class identifier in table tag

--- public-html/pages/user.php
+++ public-html/pages/user.php
@@ -22,7 +22,7 @@
         <!-- End of Topbar -->
     <div class="card-body">
       <div class="table-responsive">
-		    <table "table table-bordered" id="dataTable" width="100%" cellspacing="0">
+		    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
           <thead>
           <tr>
             <th class="center">Course Name</th>
