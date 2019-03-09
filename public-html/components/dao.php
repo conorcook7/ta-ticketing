c9e111d403d9d8b0c0ed2c4322a8ef0af2d33ad5
hphothong <hphothong@live.com>
2019-03-08 23:53:34 -0700

    Adding all data dereferencing in getClosedTickets() method

--- public-html/components/dao.php
+++ public-html/components/dao.php
@@ -547,24 +547,81 @@ class Dao {
     }
     
     /**
-     * NEED TO FINISH
-     * 
-     * Get all of the closed tickets.
-     * 
+     * Get all of the closed tickes.
      * @return $closedTickets - The array of arrays of closed tickets information.
      */
     public function getClosedTickets() {
         $conn = $this->getConnection();
-        $query = $conn->prepare("SELECT closed_ticket_id, course_name, CT.update_date, CT.available_course_id, first_name, last_name,
-         permission_id, description FROM Closed_Tickets CT JOIN Available_Courses AC ON CT.available_course_id=AC.available_course_id
-        JOIN Users U ON CT.creator_user_id=U.user_id ORDER BY CT.update_date;");
+        $query = $conn->prepare(
+            "SELECT
+                CT.closed_ticket_id,
+                CT.description,
+                CT.node_number,
+                CT.room_number,
+                CT.create_date,
+                CT.update_date,
+
+                AC.available_course_id,
+                AC.course_name,
+                AC.course_number,
+                AC.section,
+
+                Students.user_id AS student_user_id,
+                Students.permission_id AS student_permission_id,
+                Students.permission_name AS student_permission_name,
+                Students.online AS student_online,
+                Students.email AS student_email,
+                Students.first_name AS student_first_name,
+                Students.last_name AS student_last_name,
+
+                Ta.user_id AS ta_user_id,
+                Ta.permission_id AS ta_permission_id,
+                Ta.permission_name AS ta_permission_name,
+                Ta.online AS ta_online,
+                Ta.email AS ta_email,
+                Ta.first_name AS ta_first_name,
+                Ta.last_name AS ta_last_name
+            FROM
+                Closed_Tickets AS CT
+            JOIN
+                Available_Courses AS AC
+            ON CT.available_course_id = AC.available_course_id
+            JOIN
+                (SELECT
+                    Permissions.permission_id,
+                    Permissions.permission_name,
+                    user_id,
+                    online,
+                    email,
+                    first_name,
+                    last_name
+                FROM Users JOIN Permissions
+                ON Users.permission_id = Permissions.permission_id
+                ) AS Students
+            ON CT.creator_user_id = Students.user_id
+            JOIN
+                (SELECT
+                    Permissions.permission_id,
+                    Permissions.permission_name,
+                    user_id,
+                    online,
+                    email,
+                    first_name,
+                    last_name
+                FROM Users JOIN Permissions
+                ON Users.permission_id = Permissions.permission_id
+                ) AS Ta
+            ON CT.closer_user_id = Ta.user_id
+            ORDER BY CT.update_date
+            ;"
+        );
         $query->setFetchMode(PDO::FETCH_ASSOC);
         $query->execute();
         $closedTickets = $query->fetchAll();
-        $this->logger->logDebug(__FUNCTION__ . ": First closed ticket: " . print_r($openTickets[0],1));
+        $this->logger->logDebug(__FUNCTION__);
         return $closedTickets;
     }
-    
+
     /**
      * Opens a closed ticket by moving it from the closed table to the open ticket table.
      * 
