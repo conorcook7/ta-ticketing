1c0b6196168c47ddced04ce6c9034bda8714206e
hphothong <hphothong@live.com>
2019-03-10 16:32:43 -0600

    Fixing relative pathing to server pathing

--- public-html/components/header.php
+++ public-html/components/header.php
@@ -15,14 +15,14 @@
   <title>TA-Ticketing - Dashboard</title>
 
   <!-- Custom fonts for this template-->
-  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo $_SERVER['HOST'] . '/vendor/fontawesome-free/css/all.min.css'; ?>" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="../css/sb-admin-2.css" rel="stylesheet">
+  <link href="<?php echo $_SERVER['HOST'] . '/css/sb-admin-2.css'; ?>" rel="stylesheet">
 
   <!-- Custom styles for this page -->
-  <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="<?php echo $_SERVER['HOST'] . '/vendor/datatables/dataTables.bootstrap4.min.css'; ?>" rel="stylesheet">
 </head>
 
 <body id="page-top">
