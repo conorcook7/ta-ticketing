666763ccfe4dc963353d6f23925ab0eaf782ccab
hphothong <hphothong@live.com>
2019-03-10 16:32:57 -0600

    Changing help to new-ticket page

--- public-html/pages/help/new-ticket.php
+++ public-html/pages/help/new-ticket.php
@@ -1,6 +1,6 @@
 <?php
-  require_once '../components/header.php';
-  $page = 'help.php';
+  require_once '../../components/header.php';
+  $page = 'new-ticket.php';
   $nav = 'help';
 ?>
 
@@ -10,7 +10,7 @@
 
 
     <!-- Start of Sidebar -->
-    <?php include_once '../components/sidebar.php'; ?>
+    <?php include_once '../../components/sidebar.php'; ?>
     <!-- End of Sidebar -->
 
 
@@ -24,22 +24,18 @@
         <!-- Begin Page Content -->
         <div class="container-fluid p-4">
 
-          <!-- Page Heading -->
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Help</h1>
-          </div>
-
-          <!-- Submitting a ticket overview -->
+          <!-- Submitting a ticket heading -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Project Overview</h6>
+                <h1 class="h2 m-0 font-weight-bold text-primary">Creating a new ticket</h1>
             </div>
             <div class="card-body">
-                <p>The purpose of this website is to assist teaching assistants in more
-                efficiently helping students in need.
-                </p>
-                <p></p>
                 <p>
+                There are three easy steps that you need to follow in order to create
+                a new ticket. After creation, these tickets are queued for available teaching
+                assistants with the specific course you need help with. One thing to note is
+                that your ticket will be taken out of the queue if you leave the site, and
+                it will be placed at the end of the queue when you return to the site.
                 </p>
             </div>
           </div>
@@ -114,7 +110,7 @@
       <!-- End of Main Content -->
 
       <!-- Footer -->
-      <?php include_once '../components/footer.php' ?>
+      <?php include_once '../../components/footer.php' ?>
       <!-- End of Footer -->
 
     </div>
@@ -148,21 +144,21 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="../vendor/jquery/jquery.min.js"></script>
-  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../../vendor/jquery/jquery.min.js"></script>
+  <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="../js/sb-admin-2.min.js"></script>
+  <script src="../../js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="../vendor/chart.js/Chart.min.js"></script>
+  <script src="../../vendor/chart.js/Chart.min.js"></script>
 
   <!-- Page level custom scripts -->
-  <script src="../js/demo/chart-area-demo.js"></script>
-  <script src="../js/demo/chart-pie-demo.js"></script>
+  <script src="../../js/demo/chart-area-demo.js"></script>
+  <script src="../../js/demo/chart-pie-demo.js"></script>
 
 </body>
 
