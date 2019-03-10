77debbe047bf11c11c5430557e42cd50ef214a32
hphothong <hphothong@live.com>
2019-03-10 17:56:44 -0600

    changing FAQ to dynamically add questions from db

--- public-html/pages/help/faq.php
+++ public-html/pages/help/faq.php
@@ -1,4 +1,5 @@
 <?php
+  require_once '../../components/dao.php';
   require_once '../../components/header.php';
   $page = 'faq.php';
   $nav = 'help';
@@ -24,84 +25,43 @@
         <!-- Begin Page Content -->
         <div class="container-fluid p-4">
 
-          <!-- Submitting a ticket heading -->
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h1 class="h2 m-0 font-weight-bold text-primary">Creating a new ticket</h1>
-            </div>
-            <div class="card-body">
-                <p>
-                There are three easy steps that you need to follow in order to create
-                a new ticket. After creation, these tickets are queued for available teaching
-                assistants with the specific course you need help with. One thing to note is
-                that your ticket will be taken out of the queue if you leave the site, and
-                it will be placed at the end of the queue when you return to the site.
-                </p>
-            </div>
+          <!-- FAQ heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Frequently Asked Questions</h1>
           </div>
 
-          <!-- Submitting a ticket -->
-          <div class="row">
-
-            <!-- Step 1 - Submit Button -->
-            <div class="col-xl-4 col-md-6 mb-4">
-              <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2 mb-4">
-                      <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 1</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">New Ticket Button</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-plus-square fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                  <p>Create a new ticket with the "New Ticket" button on your dashboard.</p>
-                </div>
-              </div>
-            </div>
-
-            <!-- Step 2 - Information -->
-            <div class="col-xl-4 col-md-6 mb-4">
-              <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2 mb-4">
-                      <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 2</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">Fill in Information</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-comments fa-2x text-gray-300"></i>
+          <?php
+            $dao = new Dao('Dummy_TA_Ticketing');
+            $FAQs = $dao->getFAQs();
+            if (isset($FAQs)) {
+                for ($i = 0; $i < count($FAQs); $i++) {
+          ?>
+                <!-- Step 1 - Submit Button -->
+                <div class="mb-4">
+                  <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                          <div class="col mr-2 mb-4">
+                            <div
+                              class="h5 font-weight-bold text-primary text-uppercase mb-1"
+                            >FAQ #<?php echo ($i + 1); ?></div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                              <?php echo htmlentities($FAQs[$i]['question']); ?>
+                            </div>
+                          </div>
+                        </div>
+                        <p><?php echo htmlentities($FAQs[$i]['answer']); ?></p>
                     </div>
                   </div>
-                  <p>Write about your issue so that the teaching assistant can start to become
-                  familiar with your problem.
-                  </p>
                 </div>
-              </div>
-            </div>
-
-            <!-- Step 3 - Wait for assistance -->
-            <div class="col-xl-4 col-md-6 mb-4">
-              <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2 mb-4">
-                      <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 3</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">Wait for Assistance</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-clock fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                  <p>Continue working on other part of the homework, while a teaching assistant
-                  becomes available.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-          </div>
+          <?php
+                }
+            } else {
+              echo '<div class="d-sm-flex align-items-center justify-content-between mb-4">
+                      <h1 class="h3 mb-0 text-gray-800">Sorry! There are no FAQs at this time!</h1>
+                    </div>';
+            }
+          ?>
 
         </div>
         <!-- /.container-fluid -->
