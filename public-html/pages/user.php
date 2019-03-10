66a1d65af52513ce704bc542792d4b5fd2f93481
msanchez563 <michaelsanchez563@u.boisestate.edu>
2019-03-10 15:42:43 -0600

    Added title for courses table

--- public-html/pages/user.php
+++ public-html/pages/user.php
@@ -20,6 +20,11 @@
         <!-- Topbar -->
         <?php include_once '../components/topbar.php'; ?>
         <!-- End of Topbar -->
+
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">Available Courses</h6>
+    </div>
     <div class="card-body">
       <div class="table-responsive">
 		    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -63,6 +68,7 @@
 			<?php } ?>
 		  </tbody>
 		</table><br />
+  </div>
   </div>
 </div>
 		<table class = "table">
