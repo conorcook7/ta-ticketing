c93eeb56c2a5a66c0eb198cb6d8c0cdac0667117
MalikHerring <malikherring@u.boisestate.edu>
2019-03-12 06:21:59 -0700

    Moved the users table to its own file, and updated the admin page

--- public-html/pages/admin.php
+++ public-html/pages/admin.php
@@ -21,29 +21,7 @@
         <?php include_once '../components/topbar.php'; ?>
         <!-- End of Topbar -->
         
-        <table class="table">
-          <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Permission Level</th>
-            <th scope="col">User ID</th>
-          </tr>
-          </thead>
-          <tbody>
-          <?php 
-            $users = $dao->getUsers();
-            $count = 1;
-            foreach($users as $user) { ?>
-            <tr>
-              <td><?php echo htmlspecialchars($user['first_name']) . " " . htmlspecialchars($user['last_name']); ?></td>
-              <td><?php echo htmlspecialchars($user['email']); ?></td>
-              <td><?php echo $user['permission_id']; ?></td>
-              <td><?php echo $user['user_id']; ?></td>
-            </tr>
-          <?php }?>
-          </tbody>
-        </table>
+        <?php include_once '../components/admin/users-table.php'?>
       </div>
     </div>
   </div>
