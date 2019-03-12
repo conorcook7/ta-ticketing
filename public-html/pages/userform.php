c020573cc5af2021910387dd6dfc254d7b13022e
msanchez563 <michaelsanchez563@u.boisestate.edu>
2019-03-11 22:23:23 -0600

    Added page for user form submission

---
+++ public-html/pages/userform.php
@@ -0,0 +1,25 @@
+<?php
+  require_once '../components/header.php';
+  require_once '../components/dao.php';
+  $dao = new Dao("Dummy_TA_Ticketing")?>
+
+  <div id="wrapper">
+
+    <!-- Start of Sidebar -->
+    <?php include_once '../components/sidebar.php'; ?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+
+
+        <!-- Topbar -->
+        <?php include_once '../components/topbar.php'; ?>
+      </div>
+    </div>
+  </div>
+  <?php require_once '../components/footer.php';require_once '../components/scripts.php'; ?>
