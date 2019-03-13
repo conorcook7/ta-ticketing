73aa09610844de3040d8383ed43e8f97d614f4f1
Hayden Phothong <hphothong@live.com>
2019-03-13 17:22:40 -0600

    updating url in logout handler

--- public-html/handlers/logout-handler.php
+++ public-html/handlers/logout-handler.php
@@ -2,6 +2,7 @@
     require_once "../auth/google-api-php-client-2.2.2_PHP54/vendor/autoload.php";
     require_once "../components/dao.php";
     require_once "../components/KLogger.php";
+    require_once "../components/functions.php";
 
     session_start();
 
@@ -11,7 +12,7 @@
     $googleClient = new Google_Client();
     $googleClient->setClientId("153288048540-sogdggkb32ugai855a0uffo0d7h2hqnq.apps.googleusercontent.com");
     $googleClient->setClientSecret("ZyXV3mVUVs89rDkuq8RjFaH4");
-    $googleClient->setRedirectUri("http://taticketing.boisestate.edu/auth/google-auth/google.php");
+    $googleClient->setRedirectUri(generateUrl("/auth/google-auth/google.php"));
     $googleClient->setScopes("email profile");
     $googleClient->setAccessToken($_SESSION["user"]["accessToken"]);
     $googleClient->revokeToken();
