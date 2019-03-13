408d4dd72df5df9a826e701334d389256f4d4973
hphothong <hphothong@live.com>
2019-03-12 21:27:28 -0600

    Changing server pathing

--- public-html/components/sidebar.php
+++ public-html/components/sidebar.php
@@ -4,10 +4,10 @@
       <!-- Sidebar - Brand -->
       <a
         class="sidebar-brand d-flex align-items-center justify-content-center"
-        href="<?php echo $_SERVER['HOST'] . '/pages/index.php';?>"
+        href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/pages/index.php';?>"
       >
         <div class="sidebar-brand-icon">
-          <img src="<?php echo $_SERVER['HOST'] . '/img/bsu_logo.png';?>" alt="" id="logo">
+          <img src="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/img/bsu_logo.png';?>" alt="" id="logo">
         </div>
         <div class="sidebar-brand-text mx-2">Boise State</div>
       </a>
@@ -17,7 +17,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item <?php if($nav == 'dashboard'){echo 'active';}?>">
-        <a class="nav-link" href="<?php echo $_SERVER['HOST'] . '/pages/index.php';?>">
+        <a class="nav-link" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/pages/index.php';?>">
           <i class="fas fa-fw fa-home"></i>
           <span>Dashboard</span></a>
       </li>
@@ -41,11 +41,11 @@
             <h6 class="collapse-header">Custom Components:</h6>
             <a
               class="collapse-item <?php if($page == 'buttons.php'){echo 'active';}?>"
-              href="<?php echo $_SERVER['HOST'] . '/examples/buttons.php';?>"
+              href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/buttons.php';?>"
             >Buttons</a>
             <a
               class="collapse-item <?php if($page == 'cards.php'){echo 'active';}?>"
-              href="<?php echo $_SERVER['HOST'] . '/examples/cards.php';?>"
+              href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/cards.php';?>"
             >Cards</a>
           </div>
         </div>
@@ -62,19 +62,19 @@
             <h6 class="collapse-header">Custom Utilities:</h6>
             <a
               class="collapse-item <?php if($page == 'utilities-color.php'){echo 'active';}?>"
-              href="<?php echo $_SERVER['HOST'] . '/examples/utilities-color.php';?>"
+              href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/utilities-color.php';?>"
             >Colors</a>
             <a
               class="collapse-item <?php if($page == 'utilities-border.php'){echo 'active';}?>"
-              href="<?php echo $_SERVER['HOST'] . '/examples/utilities-border.php';?>"
+              href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/utilities-border.php';?>"
             >Borders</a>
             <a
               class="collapse-item <?php if($page == 'utilities-animation.php'){echo 'active';}?>"
-              href="<?php echo $_SERVER['HOST'] . '/examples/utilities-animation.php';?>"
+              href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/utilities-animation.php';?>"
             >Animations</a>
             <a
               class="collapse-item <?php if($page == 'utilities-other.php'){echo 'active';}?>"
-              href="<?php echo $_SERVER['HOST'] . '/examples/utilities-other.php';?>"
+              href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/utilities-other.php';?>"
             >Other</a>
           </div>
         </div>
@@ -98,27 +98,27 @@
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Login Screens:</h6>
             <a
-              class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/login.php';?>">Login</a>
+              class="collapse-item" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/pages/login.php';?>">Login</a>
             <a
-              class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/examples/forgot-password.php';?>">Forgot Password</a>
+              class="collapse-item" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/forgot-password.php';?>">Forgot Password</a>
             <div class="collapse-divider"></div>
             <h6 class="collapse-header">Other Pages:</h6>
-            <a class="collapse-item <?php if($page == '404.php'){echo 'active';}?>" href="<?php echo $_SERVER['HOST'] . '/examples/404.php';?>">404 Page</a>
-            <a class="collapse-item <?php if($page == 'blank.php'){echo 'active';}?>" href="<?php echo $_SERVER['HOST'] . '/examples/blank.php';?>">Blank Page</a>
+            <a class="collapse-item <?php if($page == '404.php'){echo 'active';}?>" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/404.php';?>">404 Page</a>
+            <a class="collapse-item <?php if($page == 'blank.php'){echo 'active';}?>" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/blank.php';?>">Blank Page</a>
           </div>
         </div>
       </li>
 
       <!-- Nav Item - Charts -->
       <li class="nav-item <?php if($nav == 'charts.php'){echo 'active';}?>">
-        <a class="nav-link" href="<?php echo $_SERVER['HOST'] . '/examples/charts.php';?>">
+        <a class="nav-link" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/charts.php';?>">
           <i class="fas fa-fw fa-chart-area"></i>
           <span>Charts</span></a>
       </li>
 
       <!-- Nav Item - Tables -->
       <li class="nav-item <?php if($nav == 'tables.php'){echo 'active';}?>">
-        <a class="nav-link" href="<?php echo $_SERVER['HOST'] . '/examples/tables.php';?>">
+        <a class="nav-link" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/examples/tables.php';?>">
           <i class="fas fa-fw fa-table"></i>
           <span>Tables</span></a>
       </li>
@@ -136,11 +136,11 @@
           <div class="bg-white py-2 collapse-inner rounded">
             <a 
               class="collapse-item <?php if($page == 'new-ticket.php'){echo 'active';}?>"
-              href="<?php echo $_SERVER['HOST'] . '/pages/help/new-ticket.php';?>"
+              href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/pages/help/new-ticket.php';?>"
             >Create a ticket</a>
             <a
               class="collapse-item <?php if($page == 'faq.php'){echo 'active';}?>"
-              href="<?php echo $_SERVER['HOST'] . '/pages/help/faq.php';?>"
+              href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/pages/help/faq.php';?>"
             >FAQs</a>
           </div>
         </div>
@@ -148,7 +148,7 @@
 
       <!-- About Page -->
       <li class="nav-item <?php if ($nav == 'about') { echo 'active'; }?>">
-        <a class="nav-link" href="<?php echo $_SERVER['HOST'] . '/pages/about.php';?>">
+        <a class="nav-link" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/pages/about.php';?>">
             <i class="fas fa-fw fa-info"></i>
             <span>About</span>
         </a>
