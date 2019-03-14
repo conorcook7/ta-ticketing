55b42d9f07f399f45b5c078d185de5562c47f017
Hayden Phothong <hphothong@live.com>
2019-03-13 18:19:40 -0600

    updating admin sidebar to use generateUrl function

--- public-html/components/admin/admin-sidebar.php
+++ public-html/components/admin/admin-sidebar.php
@@ -1,4 +1,8 @@
+<?php
+  require_once "../server-functions.php";
+?>
       <!-- Nav Item - Admin Collapse Menu -->
+
       <li class="nav-item <?php if($nav == 'admin'){echo 'active';}?>">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAdmin" aria-expanded="true" aria-controls="collapsePages">
           <i class="fas fa-fw fa-folder"></i>
@@ -8,19 +12,19 @@
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Main Page</h6>
             <a
-              class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin.php';?>">Admin Page</a>
+              class="collapse-item" href="<?php echo generateUrl('/pages/admin.php';?>">Admin Page</a>
             <div class="collapse-divider"></div>
             <h6 class="collapse-header">Tables</h6>
-            <a id="users" class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin.php';?>">Users</a>
-            <a id="online-users" class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin.php';?>">Online Users</a>
-            <a id="tickets" class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin.php';?>">Tickets</a>
-            <a id="open-tickets" class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin.php';?>">Open Tickets</a>
-            <a id="closed-tickets" class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin.php';?>">Closed Tickets</a>
+            <a id="users" class="collapse-item" href="<?php echo generateUrl('/pages/admin.php');?>">Users</a>
+            <a id="online-users" class="collapse-item" href="<?php echo generateUrl('/pages/admin.php');?>">Online Users</a>
+            <a id="tickets" class="collapse-item" href="<?php echo generateUrl('/pages/admin.php');?>">Tickets</a>
+            <a id="open-tickets" class="collapse-item" href="<?php echo generateUrl('/pages/admin.php');?>">Open Tickets</a>
+            <a id="closed-tickets" class="collapse-item" href="<?php echo generateUrl('/pages/admin.php');?>">Closed Tickets</a>
             <div class="collapse-divider"></div>
             <h6 class="collapse-header">Updates</h6>
-            <a id="users" class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin-updates.php';?>">Classes</a>
-            <a id="online-users" class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin-updates.php';?>">Users</a>
-            <a id="tickets" class="collapse-item" href="<?php echo $_SERVER['HOST'] . '/pages/admin-updates.php';?>">FAQs</a>
+            <a id="users" class="collapse-item" href="<?php echo generateUrl('/pages/admin-updates.php');?>">Classes</a>
+            <a id="online-users" class="collapse-item" href="<?php echo generateUrl('/pages/admin-updates.php');?>">Users</a>
+            <a id="tickets" class="collapse-item" href="<?php echo generateUrl('/pages/admin-updates.php');?>">FAQs</a>
             
           </div>
         </div>
