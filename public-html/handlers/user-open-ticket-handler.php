95466714039bf5d332243d0c64080937f72cd5e7
Hayden Phothong <hphothong@live.com>
2019-03-14 03:31:37 -0600

    commenting out statements for testing

--- public-html/handlers/user-open-ticket-handler.php
+++ public-html/handlers/user-open-ticket-handler.php
@@ -4,17 +4,17 @@
     require_once "../components/KLogger";
     
     $dao = new Dao("Dummy_TA_Ticketing");
+
+    // if (isset($_POST["open_ticket_id"]) && isset($_SESSION["user"]["user_id"])) {
+    //     $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"]);
+    // } else {
+    //     $status = 0;
+    // }
     
-    if (isset($_POST["open_ticket_id"]) && isset($_SESSION["user"]["user_id"])) {
-        $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"]);
-    } else {
-        $status = 0;
-    }
-    
-    if (!$status) {
-        $logger = new KLogger("/var/log/taticketing/", KLogger::DEBUG);
-        $logger->logError(__FILE__ . ": Unable to delete open ticket {" . $_POST["open_ticket_id"] . "}");
-    }
+    // if (!$status) {
+    //     $logger = new KLogger("/var/log/taticketing/", KLogger::DEBUG);
+    //     $logger->logError(__FILE__ . ": Unable to delete open ticket {" . $_POST["open_ticket_id"] . "}");
+    // }
 
     header("Location: ../pages/user.php");
     exit();
