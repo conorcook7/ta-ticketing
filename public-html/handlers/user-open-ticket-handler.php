2fbd510fdcdb981965cbfd51ba7bf26d513b029b
Hayden Phothong <hphothong@live.com>
2019-03-14 03:15:24 -0600

    adding missing semicolon

--- public-html/handlers/user-open-ticket-handler.php
+++ public-html/handlers/user-open-ticket-handler.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     require_once "../components/dao.php";
-    require_once "../components/KLogger"
+    require_once "../components/KLogger";
 
     $dao = new Dao("Dummy_TA_Ticketing");
     $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"]);
