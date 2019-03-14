91637dea09bf97374df588c9bac9a5b51ed62bff
Hayden Phothong <hphothong@live.com>
2019-03-13 20:51:02 -0600

    updating to absolute pathing

--- public-html/components/scripts.php
+++ public-html/components/scripts.php
@@ -1,3 +1,7 @@
+<?php
+  require_once "server-functions.php";
+?>
+
   <!-- Scroll to Top Button-->
   <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
@@ -8,29 +12,29 @@
   <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
 
   <!-- Bootstrap core JavaScript-->
-  <script src="../vendor/jquery/jquery.min.js"></script>
-  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo generateUrl('/vendor/jquery/jquery.min.js');?>"></script>
+  <script src="<?php echo generateUrl('/vendor/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?php echo generateUrl('/vendor/jquery-easing/jquery.easing.min.js');?>"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="../js/sb-admin-2.min.js"></script>
+  <script src="<?php echo generateUrl('/js/sb-admin-2.min.js');?>"></script>
 
   <!-- Page level plugins -->
-  <script src="../vendor/chart.js/Chart.min.js"></script>
+  <script src="<?php echo generateUrl('/vendor/chart.js/Chart.min.js');?>"></script>
 
   <!-- Page level custom scripts -->
-  <!-- <script src="../js/demo/chart-area-demo.js"></script> -->
-  <!-- <script src="../js/demo/chart-pie-demo.js"></script> -->
-  <script src="../js/demo/datatables-demo.js"></script>
+  <!-- <script src="<?php echo generateUrl('/js/demo/chart-area-demo.js');?>"></script> -->
+  <!-- <script src="<?php echo generateUrl('/js/demo/chart-pie-demo.js');?>"></script> -->
+  <script src="<?php echo generateUrl('/js/demo/datatables-demo.js');?>"></script>
 
   <!-- Page level plugins -->
-  <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
-  <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  <script src="<?php echo generateUrl('/vendor/datatables/jquery.dataTables.min.js');?>"></script>
+  <script src="<?php echo generateUrl('/vendor/datatables/dataTables.bootstrap4.min.js');?>"></script>
 
   <!-- Interaction for admin sidebar so the we don't load everything -->
-  <script src="../js/admin-interaction.js"></script>
+  <script src="<?php echo generateUrl('/js/admin-interaction.js');?>"></script>
 
 </body>
 </html>
