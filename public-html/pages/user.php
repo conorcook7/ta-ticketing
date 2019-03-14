e81746435fba44476d9938a527c03355d70b4dcd
Hayden Phothong <hphothong@live.com>
2019-03-14 01:11:12 -0600

    adding font awesome icon

--- public-html/pages/user.php
+++ public-html/pages/user.php
@@ -26,7 +26,7 @@
     <div class="d-sm-flex align-items-center justify-content-between">
       <h6 class="h3 mb-0 text-gray-800">Available Courses</h6>
       <form action = "userform.php">
-      <button type="submit" class="d-none d-sm-inline-block btn btn-primary">CreateNewTicket</button>
+        <button type="submit" class="d-none d-sm-inline-block btn btn-success"><i class="fas fa-create fa-sm text-white-50"></i>Create New Ticket</button>
       </form>
     </div>
   </div>
