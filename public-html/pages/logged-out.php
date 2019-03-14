dd5c1e848480670067f8cfe32a406c32557d6d7f
Hayden Phothong <hphothong@live.com>
2019-03-13 18:34:43 -0600

    making logged out bigger

--- public-html/pages/logged-out.php
+++ public-html/pages/logged-out.php
@@ -42,7 +42,7 @@
 
           <!-- 404 Error Text -->
           <div class="text-center">
-            <div class="mx-auto mb-5">You have been logged out.</div>
+            <div class="h1 mx-auto mt-5 mb-5">You have been logged out.</div>
             <p class="text-gray-500 mb-0">Please log in to your boise state account to continue.</p>
             <a href="<?php echo generateUrl("/auth/google-auth/google.php"); ?>">&larr; Log in</a>
           </div>
