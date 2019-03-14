b8246812c332eb78f234270c1b839867e7b5314e
Hayden Phothong <hphothong@live.com>
2019-03-14 01:40:51 -0600

    adding course number to the get open tickets

--- public-html/components/dao.php
+++ public-html/components/dao.php
@@ -499,9 +499,24 @@ class Dao {
      */
     public function getOpenTickets() {
         $conn = $this->getConnection();
-        $query = $conn->prepare("SELECT open_ticket_id, course_name, OT.update_date, OT.available_course_id, first_name, last_name, node_number, online, 
-        description FROM Open_Tickets OT JOIN Available_Courses AC ON OT.available_course_id=AC.available_course_id
-        JOIN Users U ON OT.creator_user_id=U.user_id ORDER BY OT.update_date;");
+        $query = $conn->prepare(
+            "SELECT
+                open_ticket_id,
+                course_name,
+                course_number,
+                OT.update_date,
+                OT.available_course_id,
+                first_name,
+                last_name,
+                node_number,
+                online, 
+                description
+            FROM Open_Tickets OT
+            JOIN Available_Courses AC ON OT.available_course_id=AC.available_course_id
+            JOIN Users U ON OT.creator_user_id=U.user_id
+            ORDER BY OT.update_date
+            ;"
+        );
         $query->setFetchMode(PDO::FETCH_ASSOC);
         $query->execute();
         $openTickets = $query->fetchAll();
@@ -522,7 +537,6 @@ class Dao {
                 OT.open_ticket_id,
                 OT.description,
                 OT.create_date,
-                AC.course_name,
                 AC.course_number
             FROM
                 Open_Tickets AS OT
