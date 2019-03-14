f6ad7296770e5d8f93d159d4f9b45d43fe20e7f6
MalikHerring <malikherring@u.boisestate.edu>
2019-03-14 03:15:52 -0600

    changed online users table

--- public-html/components/users/online-users-table.php
+++ public-html/components/users/online-users-table.php
@@ -1,24 +1,51 @@
 <div class="container-fluid">
-    <table class="table">
-    <thead>
-    <tr>
-        <th scope="col">Name</th>
-        <th scope="col">Email</th>
-        <th scope="col">Permission Level</th>
-        <th scope="col">User ID</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php 
-        $users = $dao->getOnlineUsers();
-        foreach($users as $user) { ?>
-        <tr>
-        <td><?php echo htmlspecialchars($user['first_name']) . " " . htmlspecialchars($user['last_name']); ?></td>
-        <td><?php echo htmlspecialchars($user['email']); ?></td>
-        <td><?php echo $user['permission_id']; ?></td>
-        <td><?php echo $user['user_id']; ?></td>
-        </tr>
-    <?php }?>
-    </tbody>
-    </table>
+        <!-- All Users Table -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">All Users</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+              <table class="table table-bordered data_table" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th class="center">First Name</th>
+                      <th class="center">Last Name</th>
+                      <th class="center">Email</th>
+                      <th class="center">Permission Level</th>
+                      <th class="center">User ID</th>
+                      <th class="center">Update User</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php 
+                    $users = $dao->getOnlineUsers();
+                    foreach($users as $user) { 
+                  ?>
+                        <tr>
+                        <form method="POST" action="../pages/admin.php?id=users-form">
+                            <input type="hidden" name="firstName" value="<?php echo htmlspecialchars($user['first_name']); ?>"/>
+                            <input type="hidden" name="lastName" value="<?php echo htmlspecialchars($user['last_name']); ?>"/>
+                            <input type="hidden" name="email" value="<?php echo htmlspecialchars($user['email']); ?>"/>
+                            <input type="hidden" name="permissionID" value="<?php echo $user['permission_id']; ?>"/>
+                            <input type="hidden" name="userID" value="<?php echo $user['user_id']; ?>"/>
+                            <td><?php echo htmlspecialchars($user['first_name']); ?></td>
+                            <td><?php echo htmlspecialchars($user['last_name']); ?></td>
+                            <td><?php echo htmlspecialchars($user['email']); ?></td>
+                            <td><?php echo $user['permission_id']; ?></td>
+                            <td><?php echo $user['user_id']; ?></td>
+                            <td>
+                                <button type="submit" class="btn btn-block bg-gradient-warning text-gray-100">
+                                    Update User
+                                </button>
+                            </td>
+                        </form>
+                        </tr>
+                    <?php } ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <!-- End of All Users -->
 </div>
