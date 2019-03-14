0f0f671f3273baf3416d0745746e7e26c7359a1a
MalikHerring <malikherring@u.boisestate.edu>
2019-03-14 01:14:46 -0600

    added to the create class handler

--- public-html/components/users/classes.php
+++ public-html/components/users/classes.php
@@ -1,17 +1,27 @@
+<?php if (isset($_SESSION["success"])){ ?>
+    <div class="alert alert-success">
+        <strong>Success!</strong> <?php $_SESSION["success"] ?>
+    </div>
+<?php } elseif (isset($_SESSION["failure"])) { ?>
+    <div class="alert alert-danger">
+        <strong>Failure!</strong> <?php $_SESSION["failure"] ?>
+    </div>
+<?php } ?>
+
 <div class="container">
     <form method="POST" action="<?php echo generateUrl('/handlers/create-class-handler.php')?>">
     <div class="form-group">
         <label for="courseName">Course Name</label>
-        <input type="text" class="form-control" id="courseName"  placeholder="Computer Science I">
+        <input type="text" class="form-control" id="courseName"  name="courseName" placeholder="Computer Science I" required="true">
     </div>
     <div class="form-group">
         <label for="courseNumber">Course Number</label>
-        <input type="number" class="form-control" id="courseNumber" placeholder="121">
+        <input type="number" class="form-control required" id="courseNumber" name="courseNumber" placeholder="121" required="true">
     </div>
     <div class="form-group">
         <label for="courseDescription">Course Description</label>
-        <textarea class="form-control" id="courseDescription" rows="3"></textarea>
+        <textarea class="form-control" id="courseDescription" name="courseDescription" rows="3"></textarea>
     </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Add Course</button>
     </form>
 </div>
