900122dbf254ecd740e44a12ba31784d1e4eef4a
Hayden Phothong <hphothong@live.com>
2019-03-14 03:16:48 -0600

    fixing if statement

--- public-html/handlers/user-open-ticket-handler.php
+++ public-html/handlers/user-open-ticket-handler.php
@@ -6,13 +6,10 @@
     $dao = new Dao("Dummy_TA_Ticketing");
     $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"]);
     
-    if ($status) {
-        header("Location: ../pages/user.php");
-        exit();
-
-    } else {
+    if (!$status) {
         $logger = new KLogger("/var/log/taticketing/", KLogger::DEBUG);
         $logger->logError(__FILE__ . ": Unable to delete open ticket {" . $_POST["open_ticket_id"] . "}");
-        header("Location: ../pages/user.php");
-        exit();
     }
+    
+    header("Location: ../pages/user.php");
+    exit();
