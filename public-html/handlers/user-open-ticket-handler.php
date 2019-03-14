392f1d8e23cab10b1a8dd1c655fd8251f58255bb
Hayden Phothong <hphothong@live.com>
2019-03-14 03:36:22 -0600

    removing print statements

--- public-html/handlers/user-open-ticket-handler.php
+++ public-html/handlers/user-open-ticket-handler.php
@@ -3,8 +3,6 @@
     require_once "../components/dao.php";
     require_once "../components/KLogger.php";
 
-    var_dump($_POST);
-    var_dump($_SESSION);
     $dao = new Dao("Dummy_TA_Ticketing");
 
     if (isset($_POST["open_ticket_id"]) && isset($_SESSION["user"]["user_id"])) {
