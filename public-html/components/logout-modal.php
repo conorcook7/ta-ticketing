c2f0a27f624098d71ee3087b077dbc3894238180
Hayden Phothong <hphothong@live.com>
2019-03-13 20:10:29 -0600

    formatting code

--- public-html/components/logout-modal.php
+++ public-html/components/logout-modal.php
@@ -4,22 +4,22 @@
 
 <!-- Logout Modal-->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Your active tickets will be dequeued, and queued again when you log in.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a
-            class="btn btn-primary"
-            href="<?php echo generateUrl('/handlers/logout-handler.php');?>"
-          >Logout</a>
-        </div>
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Your active tickets will be dequeued, and queued again when you log in.</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+        <a
+          class="btn btn-primary"
+          href="<?php echo generateUrl('/handlers/logout-handler.php');?>"
+        >Logout</a>
       </div>
     </div>
   </div>
+</div>
