46bc8ccc33125db1d83d42a97b16487a6a7385db
Hayden Phothong <hphothong@live.com>
2019-03-14 02:13:07 -0600

    adding values

--- public-html/components/users/users-table.php
+++ public-html/components/users/users-table.php
@@ -24,11 +24,11 @@
                   ?>
                         <tr>
                         <form method="POST" action="../pages/admin.php?id=users-form">
-                            <td name="firstName"><?php echo htmlspecialchars($user['first_name']); ?></td>
-                            <td name="lastName"><?php echo htmlspecialchars($user['last_name']); ?></td>
-                            <td name="email"><?php echo htmlspecialchars($user['email']); ?></td>
-                            <td name="permissionID"><?php echo $user['permission_id']; ?></td>
-                            <td name="userID"><?php echo $user['user_id']; ?></td>
+                            <td name="firstName" value="<?php echo htmlspecialchars($user['first_name']); ?>"><?php echo htmlspecialchars($user['first_name']); ?></td>
+                            <td name="lastName" value="<?php echo htmlspecialchars($user['last_name']); ?>"><?php echo htmlspecialchars($user['last_name']); ?></td>
+                            <td name="email" value="<?php echo htmlspecialchars($user['email']); ?>"><?php echo htmlspecialchars($user['email']); ?></td>
+                            <td name="permissionID" value="<?php echo $user['permission_id']; ?>"><?php echo $user['permission_id']; ?></td>
+                            <td name="userID" value="<?php echo $user['user_id']; ?>"><?php echo $user['user_id']; ?></td>
                             <td>
                             <button type="submit" class="btn btn-block bg-gradient-warning text-gray-100">
                                 Update User
