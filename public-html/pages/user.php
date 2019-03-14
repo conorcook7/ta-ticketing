76c4b147c936860bbe6db9e98212e9b79d816e68
Hayden Phothong <hphothong@live.com>
2019-03-14 03:37:51 -0600

    adding second table

--- public-html/pages/user.php
+++ public-html/pages/user.php
@@ -101,6 +101,89 @@
   </div>
   </div>
 </div>
+
+<div class="container-fluid">
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+    <div class="d-sm-flex align-items-center justify-content-between">
+      <h6 class="h3 mb-0 text-gray-800">My Open Tickets</h6>
+      <form action = "userform.php">
+        <button type="submit" class="d-none d-sm-inline-block btn btn-success"><i class="fas fa-plus-square fa-xl text-white pr-2"></i>Create New Ticket</button>
+      </form>
+    </div>
+  </div>
+    <div class="card-body">
+      <div class="table-responsive">
+		    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+          <tr>
+            <th class="center">Queue #</th>
+            <th class="center">Node</th>
+            <th class="center">Course Name</th>
+            <th class="center">Queue Time</th>
+            <th class="center description">Ticket Description</th>
+            <th class="center">Action</th>
+          </tr>
+		  </thead>
+		  <tbody>
+			<?php
+				$openTickets = $dao->getOpenTickets();
+				for ($i = 0; $i < sizeof($openTickets); $i++) {
+          if ($openTickets[$i]["creator_user_id"] == $_SESSION["user"]["user_id"]) {
+      ?>
+				<tr>
+          <form method="POST" action="../handlers/user-open-ticket-handler.php">
+            <input type="hidden" name="open_ticket_id" value="<?php echo $openTickets[$i]['open_ticket_id']; ?>"/>
+            <td class="center"><?php echo ($i + 1); ?></td>
+            <td class="center"><?php echo htmlspecialchars($openTickets[$i]["node_number"]); ?></td>
+            <td class="center"><?php echo strtoupper(htmlspecialchars($openTickets[$i]["course_name"])); ?></td>
+            <td class="center"><?php
+              $updateDate = new DateTime($openTickets[$i]["update_date"]);
+              echo $updateDate->format("F jS Y \a\\t g:i A");
+            ?></td>
+            <td class="center">
+              <!-- Button trigger modal -->
+              <button type="button" class="btn btn-block btn-primary" data-toggle="modal" data-target="<?php echo "#my-ticket-" . $i;?>">
+                  More Info
+              </button>
+
+              <!-- Modal -->
+              <div class="modal fade" id="<?php echo "my-ticket-" . $i; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Description</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>
+                          <div class="modal-body"><?php echo $openTickets[$i]['description']; ?></div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <th class="center">
+              <button type="submit" class="btn btn-block btn-danger">
+                <i class="fas fa-times fa-xl text-white pr-2"></i>
+                Cancel Ticket
+              </button>
+            </th>
+          </form>
+				</tr>
+      <?php
+          }
+        }
+      ?>
+		  </tbody>
+		</table><br />
+  </div>
+  </div>
+  </div>
+</div>
+
 <div class="container-fluid">
 <div class="card shadow mb-4">
   <div class="card-header py-3">
