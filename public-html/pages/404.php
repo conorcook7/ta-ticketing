5eca868d367bc072571fda5403ea8d6b97972684
Hayden Phothong <hphothong@live.com>
2019-03-13 20:00:05 -0600

    moving code to better format

--- public-html/pages/404.php
+++ public-html/pages/404.php
@@ -44,8 +44,7 @@
           <div class="text-center">
             <div class="error mx-auto" data-text="404">404</div>
             <p class="lead text-gray-800 mb-5">Page Not Found</p>
-            <a href="<?php echo generateUrl('/pages/') . strtolower($_SESSION['user']['permission']) . '.php'; ?>"
-            >&larr; Back to Dashboard</a>
+            <a href="<?php echo generateUrl('/pages/') . strtolower($_SESSION['user']['permission']) . '.php'; ?>">&larr; Back to Dashboard</a>
           </div>
 
         </div>
