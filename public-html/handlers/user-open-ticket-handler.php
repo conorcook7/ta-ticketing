c39e6771bee5dab511274dfdf255bdb49caf6c9e
Hayden Phothong <hphothong@live.com>
2019-03-14 03:26:12 -0600

    adding checks for array data

--- public-html/handlers/user-open-ticket-handler.php
+++ public-html/handlers/user-open-ticket-handler.php
@@ -2,14 +2,19 @@
     session_start();
     require_once "../components/dao.php";
     require_once "../components/KLogger";
-
+    
     $dao = new Dao("Dummy_TA_Ticketing");
-    $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"]);
+    
+    if (isset($_POST["open_ticket_id"]) && isset($_SESSION["user"]["user_id"])) {
+        $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"]);
+    } else {
+        $status = 0;
+    }
     
     if (!$status) {
         $logger = new KLogger("/var/log/taticketing/", KLogger::DEBUG);
         $logger->logError(__FILE__ . ": Unable to delete open ticket {" . $_POST["open_ticket_id"] . "}");
     }
-    
+
     header("Location: ../pages/user.php");
     exit();
