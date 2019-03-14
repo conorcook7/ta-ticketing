b687bdb18e0c2a50145cc4bd82f8c1069b30f5a1
Hayden Phothong <hphothong@live.com>
2019-03-13 18:45:23 -0600

    Changing title of page

--- public-html/pages/logged-out.php
+++ public-html/pages/logged-out.php
@@ -23,7 +23,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>TA-Ticketing - 404</title>
+  <title>Logged Out - TA Ticketing</title>
 
   <!-- Custom fonts for this template-->
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
