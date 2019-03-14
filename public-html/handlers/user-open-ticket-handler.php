258b96980610890aad0c26de9cc2c7e26dbb75fa
Hayden Phothong <hphothong@live.com>
2019-03-14 03:14:55 -0600

    adding handler for closing a ticket

--- public-html/handlers/user-open-ticket-handler.php
+++ public-html/handlers/user-open-ticket-handler.php
@@ -1,3 +1,18 @@
 <?php
+    session_start();
+    require_once "../components/dao.php";
+    require_once "../components/KLogger"
 
-    var_dump($_POST);
+    $dao = new Dao("Dummy_TA_Ticketing");
+    $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"]);
+    
+    if ($status) {
+        header("Location: ../pages/user.php");
+        exit();
+
+    } else {
+        $logger = new KLogger("/var/log/taticketing/", KLogger::DEBUG);
+        $logger->logError(__FILE__ . ": Unable to delete open ticket {" . $_POST["open_ticket_id"] . "}");
+        header("Location: ../pages/user.php");
+        exit();
+    }
