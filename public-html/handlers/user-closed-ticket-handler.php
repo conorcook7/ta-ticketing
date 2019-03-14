cde6c59a597caa1be4d0d42a10846edf8188f33c
Hayden Phothong <hphothong@live.com>
2019-03-14 04:00:43 -0600

    updating to closed ticket id

--- public-html/handlers/user-closed-ticket-handler.php
+++ public-html/handlers/user-closed-ticket-handler.php
@@ -5,7 +5,7 @@
 
     $dao = new Dao("Dummy_TA_Ticketing");
 
-    if (isset($_POST["open_ticket_id"]) && isset($_SESSION["user"]["user_id"])) {
+    if (isset($_POST["closed_ticket_id"]) && isset($_SESSION["user"]["user_id"])) {
         $status = $dao->openClosedTicket($_POST["closed_ticket_id"], $_SESSION["user"]["user_id"]);
     } else {
         $status = 0;
