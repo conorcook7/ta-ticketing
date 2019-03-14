0d0a8fdd97746399a5d909db7d88cd8aec9422cb
Hayden Phothong <hphothong@live.com>
2019-03-14 01:07:48 -0600

    removing print statements

--- public-html/handlers/user-form-handler.php
+++ public-html/handlers/user-form-handler.php
@@ -4,7 +4,6 @@ require_once '../components/dao.php';
 require_once '../components/server-functions.php';
 
 $dao = new Dao('Dummy_TA_Ticketing');
-//echo "<pre>"; print_r($_POST) ;  echo "</pre>";
 $user_id = 0;
 $course_id = 0;
 if(isset($_SESSION['user']['email'])){
@@ -13,20 +12,13 @@ if(isset($_SESSION['user']['email'])){
 }
 if(isset($_POST['courseName'])){
   $course = $dao->getAvailableCourse($_POST['courseName']);
-  echo "<pre>" . print_r($course, 1) . "</pre>";
   $course_id = $course['available_course_id'];
 }
 $nodeNum = getNodeNumber();
-echo $course_id;
-echo $_POST['description'];
-echo $_POST['courseName'];
 if(isset($_POST['courseName']) && isset($_POST['description']) && $user_id != 0 && $course_id != 0){
-    echo print_r($_POST, 1);
     $descript = $_POST['description'];
-    echo "<pre>" . $course_id . " " . $user_id . " " . $nodeNum . " " . $descript . "</pre>";
     $newTicket = $dao->createTicket($course_id,$user_id,$nodeNum, $description = $descript);
 }
-echo "<pre>" . $newTicket . "</pre>";
 if($newTicket){
   header("Location: ../pages/user.php");
   exit;
