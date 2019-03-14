b3b987b293f352cd1040ddf60b3e781ef3ca7f7a
Hayden Phothong <hphothong@live.com>
2019-03-13 20:14:06 -0600

    removing scroll to top from about.php

--- public-html/pages/about.php
+++ public-html/pages/about.php
@@ -193,11 +193,6 @@
   </div>
   <!-- End of Page Wrapper -->
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
-
 <?php
   require_once "../components/scripts.php";
 ?>
