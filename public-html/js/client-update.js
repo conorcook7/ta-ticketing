f57c1fe710a73ccddcbaa2d10552bbe8b5792d64
Hayden Phothong <hphothong@live.com>
2019-03-14 18:38:35 -0600

    changing to event listener

--- public-html/js/client-update.js
+++ public-html/js/client-update.js
@@ -1,3 +1,6 @@
-window.onbeforeunload = function() {
-    return "Do you really want to leave?";
-};
+window.addEventListener("beforeunload", function (e) {
+    var confirmationMessage = "\o/";
+  
+    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+    return confirmationMessage;                            //Webkit, Safari, Chrome
+  });
