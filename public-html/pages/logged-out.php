9133961278256b077465b2fb8586c81667191069
hphothong <hphothong@live.com>
2019-03-17 23:02:12 -0600

    aligning text center

--- public-html/pages/logged-out.php
+++ public-html/pages/logged-out.php
@@ -37,9 +37,9 @@
 
 <body id="page-top">
     <!-- Logged Out Text -->
-    <div class="flex-column d-flex align-items-center justify-content-center h-100">
+    <div class="flex-column d-flex text-center align-items-center justify-content-center h-100">
       <div class="h1 mx-auto my-5">You are signed out.</div>
-      <p class="text-gray-500 mb-0">Please use your Boise State email address to continue.</p>
+      <p class="text-gray mb-0">Please use your Boise State email address to continue.</p>
       <a id="google-sign-in" class="m-4" href="<?php echo generateUrl("/auth/google-auth/google.php"); ?>">
         <div id="google-card" class="card p-3 m-auto text-center">
           <span class="mr-auto"><img id="google-logo" src="../img/google-logo.png" alt="google-logo"/><span>
