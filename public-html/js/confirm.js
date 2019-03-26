87a2e1ba0a1ba9234bf5c84f637b043a51e8c565
conorcook7 <conorcook@u.boisestate.edu>
2019-03-26 17:48:47 -0600

    bootbox confirm adv test

--- public-html/js/confirm.js
+++ public-html/js/confirm.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
     $('.closeTickets').on('click', function(){
-        bootbox.confirm("This is the default confirm!", function(result){ 
-            console.log('This was logged in the callback: ' + result); 
+        bootbox.confirm({
+            message: "This is a confirm with custom button text and color! Do you like it?",
+            buttons: {
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'No',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function (result) {
+                console.log('This was logged in the callback: ' + result);
+            }
         });
         // bootbox.confirm({
         //     message: "Are you sure you would like to proceed?",
