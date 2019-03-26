949aec2b12b8dd8d9b474c49335ca590b25cd07c
conorcook7 <conorcook@u.boisestate.edu>
2019-03-26 17:45:28 -0600

    custom bootbox popup

--- public-html/js/confirm.js
+++ public-html/js/confirm.js
@@ -1,23 +1,22 @@
 $(document).ready(function(){
     $('.closeTickets').on('click', function(){
-        bootbox.alert("Hello! I am an alert box!!");
-        // bootbox.confirm({
-        //     message: "Are you sure you would like to proceed?",
-        //     buttons: {
-        //         confirm: {
-        //             label: '<i class="fa fa-check"></i> Confirm',
-        //             className: 'btn-success'
-        //         },
-        //         cancel: {
-        //             label: '<i class="fa fa-times"></i> Cancel',
-        //             className: 'btn-danger'
-        //         }
-        //     }
+        bootbox.confirm({
+            message: "Are you sure you would like to proceed?",
+            buttons: {
+                confirm: {
+                    label: '<i class="fa fa-check"></i> Confirm',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: '<i class="fa fa-times"></i> Cancel',
+                    className: 'btn-danger'
+                }
+            }
             // ,
             // callback: function (result) {
             //     console.log('This was logged in the callback: ' + result);
             // }
-        // });
+        });
     });
 });
 
