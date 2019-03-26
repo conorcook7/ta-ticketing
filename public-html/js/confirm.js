11e463b3805d786dff32b3d8f1062ff0e60bd55f
conorcook7 <conorcook@u.boisestate.edu>
2019-03-26 17:47:29 -0600

    bootbox generic confirm test

--- public-html/js/confirm.js
+++ public-html/js/confirm.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
     $('.closeTickets').on('click', function(){
-        bootbox.confirm({
-            message: "Are you sure you would like to proceed?",
-            buttons: {
-                confirm: {
-                    label: '<i class="fa fa-check"></i> Confirm',
-                    className: 'btn-success'
-                },
-                cancel: {
-                    label: '<i class="fa fa-times"></i> Cancel',
-                    className: 'btn-danger'
-                }
-            }
-            // ,
-            // callback: function (result) {
-            //     console.log('This was logged in the callback: ' + result);
-            // }
+        bootbox.confirm("This is the default confirm!", function(result){ 
+            console.log('This was logged in the callback: ' + result); 
         });
+        // bootbox.confirm({
+        //     message: "Are you sure you would like to proceed?",
+        //     buttons: {
+        //         confirm: {
+        //             label: '<i class="fa fa-check"></i> Confirm',
+        //             className: 'btn-success'
+        //         },
+        //         cancel: {
+        //             label: '<i class="fa fa-times"></i> Cancel',
+        //             className: 'btn-danger'
+        //         }
+        //     }
+        //     // ,
+        //     // callback: function (result) {
+        //     //     console.log('This was logged in the callback: ' + result);
+        //     // }
+        // });
     });
 });
 
