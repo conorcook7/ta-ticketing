5d4f9413c73c5fadaf475fa52782061a197d353c
conorcook7 <conorcook@u.boisestate.edu>
2019-03-26 17:40:50 -0600

    quick syntax change

--- public-html/js/confirm.js
+++ public-html/js/confirm.js
@@ -11,7 +11,8 @@ $(document).ready(function(){
                     label: '<i class="fa fa-times"></i> Cancel',
                     className: 'btn-danger'
                 }
-            },
+            }
+            // ,
             // callback: function (result) {
             //     console.log('This was logged in the callback: ' + result);
             // }
