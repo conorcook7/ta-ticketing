01ba0c0120d2f24c79ef155c9b3623d771cf03bf
conorcook7 <conorcook@u.boisestate.edu>
2019-03-26 17:42:02 -0600

    quick attempt to display alert

--- public-html/js/confirm.js
+++ public-html/js/confirm.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
     $('.closeTickets').on('click', function(){
-        bootbox.confirm({
-            message: "Are you sure you would like to proceed?",
-            buttons: {
-                confirm: {
-                    label: '<i class="fa fa-check"></i> Confirm',
-                    className: 'btn-success'
-                },
-                cancel: {
-                    label: '<i class="fa fa-times"></i> Cancel',
-                    className: 'btn-danger'
-                }
-            }
+        alert("Hello! I am an alert box!!");
+        // bootbox.confirm({
+        //     message: "Are you sure you would like to proceed?",
+        //     buttons: {
+        //         confirm: {
+        //             label: '<i class="fa fa-check"></i> Confirm',
+        //             className: 'btn-success'
+        //         },
+        //         cancel: {
+        //             label: '<i class="fa fa-times"></i> Cancel',
+        //             className: 'btn-danger'
+        //         }
+        //     }
             // ,
             // callback: function (result) {
             //     console.log('This was logged in the callback: ' + result);
             // }
-        });
+        // });
     });
 });
 
