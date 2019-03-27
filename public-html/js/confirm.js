e03b9893b4b3f33300ae9dea35f7367b767f8218
conorcook7 <conorcook@u.boisestate.edu>
2019-03-26 20:08:56 -0600

    remove comments

--- public-html/js/confirm.js
+++ public-html/js/confirm.js
@@ -18,23 +18,6 @@
                 console.log('This was logged in the callback: ' + result);
             }
         });
-        // bootbox.confirm({
-        //     message: "Are you sure you would like to proceed?",
-        //     buttons: {
-        //         confirm: {
-        //             label: '<i class="fa fa-check"></i> Confirm',
-        //             className: 'btn-success'
-        //         },
-        //         cancel: {
-        //             label: '<i class="fa fa-times"></i> Cancel',
-        //             className: 'btn-danger'
-        //         }
-        //     }
-        //     // ,
-        //     // callback: function (result) {
-        //     //     console.log('This was logged in the callback: ' + result);
-        //     // }
-        // });
     });
 }
 
