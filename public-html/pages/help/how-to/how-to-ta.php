802f42bf0954f4cdce8c0583221610a365c9b9d5
hphothong <hphothong@live.com>
2019-03-30 15:19:42 -0600

    fixing import statement for user-how to

--- public-html/pages/help/how-to/how-to-ta.php
+++ public-html/pages/help/how-to/how-to-ta.php
@@ -8,24 +8,30 @@
     }
 ?>
 
-
-    <div class="tab-pane fade <?php if ($permission == 'TA') {echo 'active show';} ?>" id="nav-how-to-ta" role="tabpanel" aria-labelledby="nav-how-to-ta-tab">
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h1 class="h2 m-0 font-weight-bold text-primary">Teaching Assistant How-To</h1>
-            </div>
-            <div class="card-body">
-                <p>
-                There are three easy steps that you need to follow in order to create
-                a new ticket. After creation, these tickets are queued for available teaching
-                assistants with the specific course you need help with. One thing to note is
-                that your ticket will be taken out of the queue if you leave the site, and
-                it will be placed at the end of the queue when you return to the site.
-                </p>
-            </div>
+<div class="tab-pane fade <?php if ($permission == 'TA') {echo 'active show';} ?>" id="nav-how-to-ta" role="tabpanel" aria-labelledby="nav-how-to-ta-tab">
+    
+    <!-- Close a ticket -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h1 class="h2 m-0 font-weight-bold text-primary">Close a ticket</h1>
+        </div>
+        <div class="card-body">
+            
         </div>
     </div>
-    <div class="tab-pane fade" id="nav-how-to-user" role="tabpanel" aria-labelledby="nav-how-to-user-tab">
-        <?php require_once "how-to/how-to-user.php"; ?>
+    
+    <!-- Open a ticket -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h1 class="h2 m-0 font-weight-bold text-primary">Open a ticket</h1>
+        </div>
+        <div class="card-body">
+            
+        </div>
     </div>
+
+</div>
+<div class="tab-pane fade" id="nav-how-to-user" role="tabpanel" aria-labelledby="nav-how-to-user-tab">
+    <?php require_once "how-to/how-to-user.php"; ?>
+</div>
   
