378b0ffaafaf47b6d039b145abec92a31026a7cf
hphothong <hphothong@live.com>
2019-03-29 22:13:20 -0600

    Adding cascading permissions to how-to

---
+++ public-html/pages/help/how-to.php
@@ -0,0 +1,103 @@
+<?php
+    session_start();
+    $page = "how-to.php";
+    $nav = "help";
+    require_once "../../components/header.php";
+?>
+<div id="wrapper">
+
+    <?php include_once '../../components/sidebar.php'; ?>
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <?php include_once '../../components/topbar.php'; ?>
+        <!-- Main Content -->
+        <div id="content" class="p-2">
+
+            <!-- Begin Page Content -->
+            <div class="container-fluid p-4">
+            
+                <?php
+                    $permission = strtoupper($_SESSION["user"]["permission"]);
+
+                    if ($permission == "ADMIN") {
+                ?>
+                        <div class="row">
+                            <div class="col-xs-12 ">
+                                <nav>
+                                    <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                                        <a
+                                            class="nav-item nav-link active"
+                                            id="nav-how-to-admin-tab"
+                                            data-toggle="tab"
+                                            href="#nav-how-to-admin"
+                                            role="tab"
+                                            aria-controls="nav-how-to-admin"
+                                            aria-selected="true"
+                                        >Admin How-To</a>
+                                        <a
+                                            class="nav-item nav-link"
+                                            id="nav-how-to-ta-tab"
+                                            data-toggle="tab"
+                                            href="#nav-how-to-ta"
+                                            role="tab"
+                                            aria-controls="nav-how-to-ta"
+                                            aria-selected="false"
+                                        >Teaching Assistant How-To</a>
+                                        <a
+                                            class="nav-item nav-link"
+                                            id="nav-how-to-user-tab"
+                                            data-toggle="tab"
+                                            href="#nav-how-to-user"
+                                            role="tab"
+                                            aria-controls="nav-how-to-user"
+                                            aria-selected="false"
+                                        >User How-To</a>
+                                    </div>
+                                </nav>
+                                <?php require_once "how-to/how-to-admin.php"; ?>
+                            </div>
+                        </div>
+                <?php  } else if ($permission == "TA") { ?>
+                        <div class="row">
+                            <div class="col-xs-12 ">
+                                <nav>
+                                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                        <a
+                                            class="nav-item nav-link active"
+                                            id="nav-how-to-ta-tab"
+                                            data-toggle="tab"
+                                            href="#nav-how-to-ta"
+                                            role="tab"
+                                            aria-controls="nav-how-to-ta"
+                                            aria-selected="false"
+                                        >Teaching Assistant How-To</a>
+                                        <a
+                                            class="nav-item nav-link"
+                                            id="nav-how-to-user-tab"
+                                            data-toggle="tab"
+                                            href="#nav-how-to-user"
+                                            role="tab"
+                                            aria-controls="nav-how-to-user"
+                                            aria-selected="false"
+                                        >User How-To</a>
+                                    </div>
+                                </nav>
+                                <?php require_once "how-to/how-to-ta.php"; ?>
+                            </div>
+                        </div>
+                <?php   } else { require_once "how-to/how-to-user.php"; } ?>
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+        <?php include_once '../../components/footer.php'; ?>
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<?php require_once "../../components/scripts.php";?>
