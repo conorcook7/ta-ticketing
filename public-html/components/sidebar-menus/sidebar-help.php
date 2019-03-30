e3199f3dc5b4f733ac4daec21f19f6028e1234a6
hphothong <hphothong@live.com>
2019-03-29 20:28:08 -0600

    adding how-to link to navbar

--- public-html/components/sidebar-menus/sidebar-help.php
+++ public-html/components/sidebar-menus/sidebar-help.php
@@ -1,6 +1,17 @@
+<?php
+    session_start();
+?>
+
 <!-- Help Page -->
 <li class="nav-item <?php if ($nav == 'help') { echo 'active'; } ?>">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseHelpPages" aria-expanded="true" aria-controls="collapseHelpPages">
+    <a
+        class="nav-link collapsed"
+        href="#"
+        data-toggle="collapse"
+        data-target="#collapseHelpPages"
+        aria-expanded="true"
+        aria-controls="collapseHelpPages"
+    >
         <i class="fas fa-fw fa-question"></i>
         <span>Help</span>
     </a>
@@ -9,7 +20,7 @@
             <a
                 class="collapse-item <?php if($nav == 'help' && $page == 'available-courses.php'){echo 'active';}?>"
                 href="<?php echo generateUrl('/pages/help/available-courses.php');?>"
-            >Available courses</a>
+            >Available Courses</a>
             <a 
                 class="collapse-item <?php if($nav == 'help' && $page == 'new-ticket.php'){echo 'active';}?>"
                 href="<?php echo generateUrl('/pages/help/new-ticket.php');?>"
@@ -18,6 +29,24 @@
                 class="collapse-item <?php if($nav == 'help' && $page == 'faq.php'){echo 'active';}?>"
                 href="<?php echo generateUrl('/pages/help/faq.php');?>"
             >FAQs</a>
+
+
+
+            <?php
+                $permission = strtoupper($_SESSION["user"]["permission"]);
+                if ($permission == "USER" || $permission == "TA" || $permission == "ADMIN") {
+            ?>
+            <a
+                <?php
+                    if($nav == 'help' && ($page == 'how-to-user.php' || $page == 'how-to-ta.php' || $page == 'how-to-admin.php')) {
+                        echo 'class="collapse-item active"';
+                    } else {
+                        echo 'class="collapse-item"';
+                    }
+                ?>
+                href="<?php echo generateUrl('/pages/help/how-to-' . strtolower($permission) . '.php'); ?>"
+            >How-To</a>
+            <?php } ?>
         </div>
     </div>
 </li>
