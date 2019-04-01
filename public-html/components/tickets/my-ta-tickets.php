f95779e1f098894e71942f07ac403f3b2d483f58
conorcook7 <conorcook@u.boisestate.edu>
2019-03-31 22:13:37 -0600

    testing.4

--- public-html/components/tickets/my-ta-tickets.php
+++ public-html/components/tickets/my-ta-tickets.php
@@ -9,7 +9,7 @@ $myTickets = $dao->getMyOpenTickets($my_course_id['available_course_id']);
         <div class="card shadow mb-4">
             <div class="card-header py-3">
               <h6 class="m-0 font-weight-bold text-primary">My Open Tickets 
-                  <?php print_r($my_course_id); echo "data: ". $my_course_id['available_course_id']; ?>
+                  <?php print_r($my_course_id); echo "data: ". $my_course_id['0']['available_course_id']; ?>
               </h6>
             </div>
             <div class="card-body">
