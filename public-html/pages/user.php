ef52de0c3aecf08c9d1303f5011acd52f4d855f4
msanchez563 <michaelsanchez563@u.boisestate.edu>
2019-04-01 00:16:48 -0600

    Changed other tables to include new pages

--- public-html/pages/user.php
+++ public-html/pages/user.php
@@ -112,12 +112,12 @@
   <div class="card shadow mb-4">
     <div class="card-header py-3">
     <div class="d-sm-flex align-items-center justify-content-between">
-      <h6 class="h3 mb-0 text-gray-800">My Closed Tickets</h6>
+      <h6 class="m-0 font-weight-bold text-primary">My Closed Tickets</h6>
     </div>
   </div>
     <div class="card-body">
       <div class="table-responsive">
-		    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+		    <table class="table table-bordered data_table" id="dataTable" width="100%" cellspacing="0">
           <thead>
           <tr>
             <th class="center">Ticket #</th>
@@ -197,7 +197,7 @@
   </div>
   <div class="card-body">
     <div class="table-responsive">
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+      <table class="table table-bordered data_table" id="dataTable" width="100%" cellspacing="0">
         <thead>
         <tr>
           <th class="center">TA Name</th>
