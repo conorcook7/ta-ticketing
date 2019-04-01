a3bcb30a21d9f9112ec66d27f3c029b9ea529b85
conorcook7 <conorcook@u.boisestate.edu>
2019-03-31 21:32:01 -0600

    testing session

--- public-html/components/tickets/my-ta-tickets.php
+++ public-html/components/tickets/my-ta-tickets.php
@@ -6,7 +6,7 @@ $my_ta_id = $_SESSION[user][user_id];
         <!-- Start of My Tickets Table -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">My Open Tickets</h6>
+              <h6 class="m-0 font-weight-bold text-primary">My Open Tickets <?php print_r($_SESSION) ?></h6>
             </div>
             <div class="card-body">
               <div class="table-responsive">
