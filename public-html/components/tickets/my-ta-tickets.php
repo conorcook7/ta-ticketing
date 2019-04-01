72f45c97eceff8d35f1fd6dfd801714495d63a43
conorcook7 <conorcook@u.boisestate.edu>
2019-03-31 21:52:02 -0600

    implement mycourseid

--- public-html/components/tickets/my-ta-tickets.php
+++ public-html/components/tickets/my-ta-tickets.php
@@ -1,12 +1,14 @@
 <?php $page = "my-ta-tickets.php";
-$myTickets = $dao->getMyOpenTickets($my_course_id);
 $my_ta_id = $_SESSION[user][user_id];
+$my_course_id = $dao->getMyCourseID($my_ta_id);
+$myTickets = $dao->getMyOpenTickets($my_course_id);
+
  ?>
 <div class="container-fluid">  
         <!-- Start of My Tickets Table -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">My Open Tickets <?php print_r($_SESSION) ?></h6>
+              <h6 class="m-0 font-weight-bold text-primary">My Open Tickets <?php print_r($my_course_id) ?></h6>
             </div>
             <div class="card-body">
               <div class="table-responsive">
