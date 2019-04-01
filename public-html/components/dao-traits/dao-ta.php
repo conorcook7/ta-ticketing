e0f3dcdd86933f2b79b503ae2a7e74e1de535426
conorcook7 <conorcook@u.boisestate.edu>
2019-03-31 22:05:29 -0600

    change dao method to get course if

--- public-html/components/dao-traits/dao-ta.php
+++ public-html/components/dao-traits/dao-ta.php
@@ -152,10 +152,7 @@ trait DaoTa {
         try {
             $conn = $this->getConnection();
             $query = "SELECT
-                        available_course_id,
-                        course_name,
-                        course_number,
-                        section
+                        available_course_id
                     FROM
                         Available_Courses AC
                     JOIN
