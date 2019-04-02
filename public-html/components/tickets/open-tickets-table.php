c443370839cd5dba86a602d5bc4525efb5697317
conorcook7 <conorcook@u.boisestate.edu>
2019-04-02 15:35:28 -0600

    testing button names

--- public-html/components/tickets/open-tickets-table.php
+++ public-html/components/tickets/open-tickets-table.php
@@ -38,7 +38,7 @@
                         <td class="center">
                             <!-- Button trigger modal -->
                             <button type="button" class="btn btn-block bg-primary text-gray-100" data-toggle="modal" data-target="#exampleModalCenter">
-                                More Info
+                                Click for Info
                             </button>
 
                             <!-- Modal -->
