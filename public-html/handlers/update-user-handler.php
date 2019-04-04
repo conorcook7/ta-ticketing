6a4204566f3e4f7873207f6a85b726c79f928aea
MalikHerring <malikherring@u.boisestate.edu>
2019-04-04 08:02:02 -0600

    added more description to reply

--- public-html/handlers/update-user-handler.php
+++ public-html/handlers/update-user-handler.php
@@ -9,9 +9,9 @@
     $email = $_POST["email"];
     $permissionID = $_POST["permissionID"];
     if($dao->updateUser($user_id, $firstName, $lastName, $email, $permissionID, $admin_id) == TRUE){
-        $_SESSION["success"] = "Updated the user: " . $name;
+        $_SESSION["success"] = "Updated the user: " . $firstName . " " . $lastName;
     } else {
-        $_SESSION["failure"] = "Failed to update the user " . $name;
+        $_SESSION["failure"] = "Failed to update the user: " . $firstName . " " . $lastName;
     }
     header("Location: ../pages/admin.php?id=users");
     exit;
