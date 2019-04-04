415f1a0cf98e3a9b41b9c892af2581fcdb53ec7a
MalikHerring <malikherring@u.boisestate.edu>
2019-04-04 07:35:55 -0600

    fixed a naming error

--- public-html/components/tickets/tickets-table.php
+++ public-html/components/tickets/tickets-table.php
@@ -1,26 +1,26 @@
-<?php $page = "tickets-table.php"; ?>
+<?php $page = "tickets-table.php";?>
 <div class="container-fluid">
-        <!-- All Open Tickets Table -->
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">All Tickets</h6>
-            </div>
-            <div class="card-body">
-              <div class="table-responsive">
-              <table class="table table-bordered data_table" id="all-tickets-table" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <th class="center">ID</th>
-                      <th class="center">Student Name</th>
-                      <th class="center nodeInfo">Node</th>
-                      <th class="center courseInfo">Course</th>
-                      <th class="center status">Status</th>
-                      <th class="center description">Description</th>
-                    </tr>
-                  </thead>
+    <!-- All Tickets Table -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">All Tickets</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered data_table" id="all-tickets-table" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th class="center">Student Name</th>
+                            <th class="center">Teaching Assistant</th>
+                            <th class="center nodeInfo">Course</th>
+                            <th class="center description">Ticket Description</th>
+                            <th class="center">Closing Remarks</th>
+                            <th class="center action">Status</th>
+                        </tr>
+                    </thead>
                 </table>
-              </div>
             </div>
-          </div>
-          <!-- End of All open tickets -->
+        </div>
+    </div>
+    <!-- End of all tickets. -->
 </div>
