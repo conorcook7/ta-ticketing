2ec032b95342f5604993f522352c296b49682125
MalikHerring <malikherring@u.boisestate.edu>
2019-04-04 08:03:19 -0600

    naming error

--- public-html/handlers/update-user-handler.php
+++ public-html/handlers/update-user-handler.php
@@ -6,7 +6,7 @@
     $user_id = $_POST["userID"];
     $firstName = $_POST["firstName"];
     $lastName = $_POST["lastName"];
-    $email = $_POST["email"];
+    $email = $_POST["userEmail"];
     $permissionID = $_POST["permissionID"];
     if($dao->updateUser($user_id, $firstName, $lastName, $email, $permissionID, $admin_id) == TRUE){
         $_SESSION["success"] = "Updated the user: " . $firstName . " " . $lastName;
