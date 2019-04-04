4d71da95af6dfea6b7cff8907eb895a9e8d1b6f4
MalikHerring <malikherring@u.boisestate.edu>
2019-04-04 07:04:56 -0600

    updated delete FAQ

--- public-html/handlers/delete-faq-handler.php
+++ public-html/handlers/delete-faq-handler.php
@@ -5,8 +5,13 @@
     $id = $_SESSION['user']['user_id'];
     $question = $_POST["question"];
     $answer = $_POST["answer"];
-    if($dao->deleteFAQ($id) == TRUE){
-        $_SESSION["success"] = "Deleted the Question: " . $question;
+    if(isset($_POST["faqID"])){
+        $faqID = $_POST["faqID"];
+        if($dao->deleteFAQ($faqID) == TRUE){
+            $_SESSION["success"] = "Deleted the Question: " . $question;
+        } else {
+            $_SESSION["failure"] = "Failed to delete the Question: " . $question;
+        }
     } else {
         $_SESSION["failure"] = "Failed to delete the Question: " . $question;
     }
