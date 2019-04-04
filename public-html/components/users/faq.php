2044a6f1d59bff05456e9f9683578c881467b43f
MalikHerring <MalikHerring@u.boisestate.edu>
2019-04-03 22:59:15 -0600

    added in pretext for faq

--- public-html/components/users/faq.php
+++ public-html/components/users/faq.php
@@ -19,11 +19,13 @@
     <legend class="border-bottom mb-4">Create FAQ</legend>
     <div class="form-group">
         <label for="question">Question</label>
-        <input type="text" class="form-control" id="question" name="question" value="<?php echo isset($_POST["question"]) ? $_POST["question"] : "What's going on?" ?>" required="true">
+        <input type="text" class="form-control" id="question" name="question" <?php echo isset($_POST["question"]) ? "value=\"" . $_POST["question"] . "\"" : "placeholder=\"Why is nothing displaying?\"" ?>required="true">
     </div>
     <div class="form-group">
         <label for="answer">Answer</label>
-        <textarea class="form-control" id="answer" name="answer" rows="3" placeholder="Try clearing your cache and cookies and if that does not work contact Ben Peterson."></textarea>
+        <textarea class="form-control" id="answer" name="answer" rows="3" placeholder="Try clearing your cache and cookies and if that does not work contact Ben Peterson.">
+        <?php echo isset($_POST["answer"]) ? $_POST["answer"]; ?>
+        </textarea>
     </div>
         <button type="submit" class="btn btn-primary">Add/Update FAQ</button>
     </form>
