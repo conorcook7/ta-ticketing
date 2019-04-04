bad89f564ca3fc5725b0cb4703108137dfe208ad
hphothong <hphothong@live.com>
2019-04-04 04:01:54 -0600

    Reverting to use old ajax

--- public-html/js/ajax/tables-ajax.js
+++ public-html/js/ajax/tables-ajax.js
@@ -7,29 +7,24 @@ $(document).ready(function() {
   }
   ajaxPath += "handlers/ajax/";
 
-  //   // All of the tables ajax calls here
-  //   let closedTicketsTable = $("#closed-tickets-table").DataTable({
-  //     ajax: ajaxPath + "closed-tickets-table-handler.php",
-  //     processing: true,
-  //     columns: [
-  //       { data: "studentName" },
-  //       { data: "teachingAssistant" },
-  //       { data: "course" },
-  //       { data: "ticketDescription" },
-  //       { data: "closingRemarks" },
-  //       { data: "status" }
-  //     ]
-  //   });
-
-  //   // Add more tables here
-  //   // TODO: Add more tables
-
-  //   // Reload the data for all tables
-  //   closedTicketsTable.ajax.reload();
-  //   // TODO: Add more table reloads here
-  $("#closed-ticket-table").DataTable({
-    ajax: ajaxPath + "close-ssp-handler.php",
+  // All of the tables ajax calls here
+  let closedTicketsTable = $("#closed-tickets-table").DataTable({
+    ajax: ajaxPath + "closed-tickets-table-handler.php",
     processing: true,
-    serverSide: true
+    columns: [
+      { data: "studentName" },
+      { data: "teachingAssistant" },
+      { data: "course" },
+      { data: "ticketDescription" },
+      { data: "closingRemarks" },
+      { data: "status" }
+    ]
   });
+
+  // Add more tables here
+  // TODO: Add more tables
+
+  // Reload the data for all tables
+  closedTicketsTable.ajax.reload();
+  // TODO: Add more table reloads her
 });
