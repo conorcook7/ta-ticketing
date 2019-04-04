2df13ada3df4ef503c7c7de0a1e489d4ebcbd71c
Hayden Phothong <hphothong@live.com>
2019-04-04 01:46:44 -0600

    changing to use hostname

--- public-html/js/ajax/tables-ajax.js
+++ public-html/js/ajax/tables-ajax.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   console.log(window.origin);
   // All of the tables ajax calls here
   let closedTicketsTable = $("#closed-tickets-table").DataTable({
-    ajax: window.origin + "/handlers/ajax/closed-tickets-table-handler.php",
+    ajax:
+      window.location.hostname +
+      "/handlers/ajax/closed-tickets-table-handler.php",
     processing: true,
     columns: [
       { data: "studentName" },
