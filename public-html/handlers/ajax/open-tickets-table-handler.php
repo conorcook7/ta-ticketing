2fa5db062c3c6a1037c6adf09f33066b7baf0537
MalikHerring <malikherring@u.boisestate.edu>
2019-04-06 11:36:50 -0600

    testing closed tickets on open tickets

--- public-html/handlers/ajax/open-tickets-table-handler.php
+++ public-html/handlers/ajax/open-tickets-table-handler.php
@@ -1,31 +1,30 @@
-<?php 
+<?php
     session_start();
     require_once "../../components/server-functions.php";
     require_once "../../components/dao.php";
 
     $logger = getServerLogger();
     $dao = new Dao();
-    $my_ta_id = $_SESSION["user"]["user_id"];
 
-    $openTickets = $dao->getOpenTickets();
+    $closedTickets = $dao->getClosedTickets();
     $cleanTickets = [
         "data" => array()
     ];
-    $queue = 0;
-    foreach ($openTickets as $ticket) {
-        $queue++;
+
+    // Sanitize the tickets
+    foreach ($closedTickets as $ticket) {
         $cleanTicket = Array();
-        $cleanTicket["queue"] = $queue;
-        $cleanTicket["studentName"] = htmlentities($ticket["first_name"] . " " . $ticket["last_name"]);
-        $cleanTicket["nodeNumber"] = $ticket['node_number'];
+        $cleanTicket["id"] = $ticket["closed_ticket_id"];
+        $cleanTicket["studentName"] = htmlentities($ticket["student_first_name"] . " " . $ticket["student_last_name"]);
+        $cleanTicket["teachingAssistant"] = htmlentities($ticket["ta_first_name"] . " " . $ticket["ta_last_name"]);
         $cleanTicket["course"] = htmlentities($ticket["course_name"]);
         $cleanTicket["ticketDescription"] = '
             <!-- Button trigger modal for ticket descriptions-->
-            <button type="button" class="btn btn-block bg-primary text-gray-100" data-toggle="modal" data-target="#ticket-description-OPEN' . $ticket["open_ticket_id"] . '">
-                More Info
+            <button type="button" class="btn btn-block bg-primary text-gray-100" data-toggle="modal" data-target="#ticket-description' . $ticket["closed_ticket_id"] . '">
+                Click Here
             </button>
             <!-- Modal for ticket descriptions-->
-            <div class="modal fade" id="ticket-description-OPEN' . $ticket["open_ticket_id"] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal fade" id="ticket-description' . $ticket["closed_ticket_id"] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -42,42 +41,60 @@
                 </div>
             </div>
         ';
-        $cleanTicket["status"] = '
-        <button type="button" class="btn btn-block bg-danger text-gray-100" data-toggle="modal" data-target="#confirmModalAllTicket'.$ticket['open_ticket_id'].'"> 
-            Close Ticket
-        </button>
-        <!-- Confirmation Modal -->
-        <div class="modal fade" id="confirmModalAllTicket'.$ticket['open_ticket_id'].'" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <form method="POST" action="../handlers/ta-handler.php" id="all_ticket_form'. $queue.'">
-            <input type="hidden" name="open_ticket_id_input" value="'. $ticket['open_ticket_id'].'"/>
-            <input type="hidden" name="closer_id_input" value="'. $my_ta_id.'"/>
-             <div class="modal-header">
-                <h5 class="modal-title">Are you sure you want to close this ticket?</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-             </div>
-              <div class="modal-body">
-                  <textarea placeholder="Please describe how you helped with this ticket..." required form="all_ticket_form'.$ticket['open_ticket_id'].'" name="limitedtextarea" class="form-control"  rows="5" onKeyDown="limitText(this.form.limitedtextarea,this.form.countdown,500);" 
-                        onKeyUp="limitText(this.form.limitedtextarea,this.form.countdown,500);"></textarea>
-                 
-               </div>
-               <div class="modal-footer">
-                  <div class="mr-auto">
-                    <span>(Maximum characters: 500)</span><br>
-                    <span>You have <input readonly type="text" name="countdown" size="3" value="500"> characters left.</span>
-                   </div>
-                  <button type="submit" class="btn btn-success">Confirm</button>
-                  <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-               </div>
-              </div>
+        $cleanTicket["closingRemarks"] = '
+            <!-- Button trigger modal for closing descriptions -->
+            <button type="button" class="btn btn-block bg-info text-gray-100" data-toggle="modal" data-target="#closed-description' . $ticket["closed_ticket_id"] . '">
+                Click Here
+            </button>
+            <!-- Modal for closing descriptions-->
+            <div class="modal fade" id="closed-description' . $ticket["closed_ticket_id"] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Why this ticket was closed...</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">' . htmlentities($ticket["closing_description"]) . '</div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+        ';
+        $cleanTicket["status"] = '
+            <button type="button" class="btn btn-block bg-success text-gray-100" data-toggle="modal" data-target="#confirmModalReOpen' . $ticket["closed_ticket_id"] . '">
+                Reopen Ticket
+            </button>
+            <!-- Confirmation Modal -->
+            <div class="modal fade" id="confirmModalReOpen' . $ticket["closed_ticket_id"] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Please Confirm</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Are you sure you want to reopen this ticket?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <form method="POST" action="../handlers/ta-handler.php"> 
+                                <input type="hidden" name="closed_ticket_id" value="' . $ticket["closed_ticket_id"] . '"/>
+                                <input type="hidden" name="opener_id" value="' . $ticket["ta_user_id"] . '"/>
+                                <button type="submit" class="btn btn-success">Confirm</button>
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         ';
         $cleanTickets["data"][] = $cleanTicket;
     }
+    
     header("Content-Type: application/json");
     echo json_encode($cleanTickets);
-?>
