85f466f415c12da1452d28b41a9fa11c675b63cc
MalikHerring <malikherring@u.boisestate.edu>
2019-04-06 13:05:01 -0600

    updated buttons

--- public-html/components/users/online-users-table.php
+++ public-html/components/users/online-users-table.php
@@ -36,7 +36,7 @@
                             <td><?php echo $user['permission_id']; ?></td>
                             <td><?php echo $user['user_id']; ?></td>
                             <td>
-                                <button type="submit" class="btn btn-block bg-gradient-warning text-gray-100">
+                                <button type="submit" class="btn btn-block bg-warning text-gray-100">
                                     Update User
                                 </button>
                             </td>
