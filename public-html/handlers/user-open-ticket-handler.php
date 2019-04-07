ea9fca4f4a4fe4a870dd2845591841bead51e2fd
Hayden Phothong <hphothong@live.com>
2019-04-07 11:06:31 -0600

    adding self close description

--- public-html/handlers/user-open-ticket-handler.php
+++ public-html/handlers/user-open-ticket-handler.php
@@ -6,7 +6,8 @@
     $dao = new Dao();
 
     if (isset($_POST["open_ticket_id"]) && isset($_SESSION["user"]["user_id"])) {
-        $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"]);
+        $closingDescription = isset($_POST["closing_description"]) ? $_POST["closing_description"] : "Self Closed";
+        $status = $dao->closeTicket($_POST["open_ticket_id"], $_SESSION["user"]["user_id"], $closingDescription);
     } else {
         $status = 0;
     }
