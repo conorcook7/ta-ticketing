74b8e5a24112afd989a384fa9f71656fef4dcfeb
Hayden Phothong <hphothong@live.com>
2019-04-08 23:58:09 -0600

    removing database strictness

--- research/scripts/sql/add-dev-team.sql
+++ research/scripts/sql/add-dev-team.sql
@@ -1,4 +1,3 @@
-USE Dummy_TA_Ticketing;
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (200, 1, 'michaelsanchez563@u.boisestate.edu', 'Michael', 'Sanchez');
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (403, 2, 'conorcook@u.boisestate.edu', 'Conor', 'Cook');
 INSERT INTO Teaching_Assistants (user_id, available_course_id, start_time_past_midnight, end_time_past_midnight) VALUES (403, 1, '08:00:00', '16:00:00');
