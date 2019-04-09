50c2bee9a5afae2260e4d48c825bc41ede38d4b7
MalikHerring <malikherring@u.boisestate.edu>
2019-04-09 07:48:32 -0600

    fixed a copy paste cosmetic error

--- public-html/handlers/update-user-handler.php
+++ public-html/handlers/update-user-handler.php
@@ -11,7 +11,7 @@
     if($user_id == -1){
         if($dao->createUser($email, $firstName, $lastName) == TRUE){
             if($dao->updateUser($user_id, $firstName, $lastName, $email, $permissionID, $admin_id) == TRUE){
-                $_SESSION["success"] = "Updated the user: " . $firstName . " " . $lastName;
+                $_SESSION["success"] = "Created the user: " . $firstName . " " . $lastName;
             } else {
                 $_SESSION["failure"] = "Failed to update the user: " . $firstName . " " . $lastName;
             }
