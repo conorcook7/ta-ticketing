99e39c2b18ffab30a8d22f2f7fa81dc0fb9b3e28
MalikHerring <malikherring@u.boisestate.edu>
2019-04-09 07:47:31 -0600

    added the add user functionality

--- public-html/handlers/update-user-handler.php
+++ public-html/handlers/update-user-handler.php
@@ -8,6 +8,24 @@
     $lastName = $_POST["lastName"];
     $email = $_POST["userEmail"];
     $permissionID = $_POST["permissionID"];
+    if($user_id == -1){
+        if($dao->createUser($email, $firstName, $lastName) == TRUE){
+            if($dao->updateUser($user_id, $firstName, $lastName, $email, $permissionID, $admin_id) == TRUE){
+                $_SESSION["success"] = "Updated the user: " . $firstName . " " . $lastName;
+            } else {
+                $_SESSION["failure"] = "Failed to update the user: " . $firstName . " " . $lastName;
+            }
+            $_SESSION["success"] = "Created the user: " . $firstName . " " . $lastName;
+        } else {
+            $_SESSION["failure"] = "Failed to create the user: " . $firstName . " " . $lastName;
+        }  
+    } else {
+        if($dao->updateUser($user_id, $firstName, $lastName, $email, $permissionID, $admin_id) == TRUE){
+            $_SESSION["success"] = "Updated the user: " . $firstName . " " . $lastName;
+        } else {
+            $_SESSION["failure"] = "Failed to update the user: " . $firstName . " " . $lastName;
+        }        
+    }
     if($dao->updateUser($user_id, $firstName, $lastName, $email, $permissionID, $admin_id) == TRUE){
         $_SESSION["success"] = "Updated the user: " . $firstName . " " . $lastName;
     } else {
