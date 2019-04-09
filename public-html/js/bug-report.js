3c099095d5f7e62c1f31b37dfd66037c9c26fbfb
Hayden Phothong <hphothong@live.com>
2019-04-08 21:57:05 -0600

    adding success message fade out

--- public-html/js/bug-report.js
+++ public-html/js/bug-report.js
@@ -5,4 +5,9 @@ $(document).ready(function() {
   description.on("change keyup mousedown paste", function() {
     $("#char-count").html(description.val().length);
   });
+
+  // Fade out the success alert.
+  setTimeout(function() {
+    $(".alert-success").fadeOut("slow");
+  }, 5 * 1000);
 });
