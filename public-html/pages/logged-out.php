0ed89d7df3b032909748d7350e6283e43b1fa649
Hayden Phothong <haydenphothong@u.boisestate.edu>
2019-04-10 18:51:07 -0600

    adding second local ip

--- public-html/pages/logged-out.php
+++ public-html/pages/logged-out.php
@@ -85,6 +85,32 @@
                 console.log(ip);
             }
         </script>
+        <script>
+            var $mytimeout;
+            if ( window.RTCPeerConnection  window.mozRTCPeerConnection  window.webkitRTCPeerConnection ) {
+                $mytimeout = setTimeout(function(){
+                    console.log('Local ip not supported');
+                },3000);
+                window.RTCPeerConnection = window.RTCPeerConnection  window.mozRTCPeerConnection  window.webkitRTCPeerConnection;
+                var $pc = new RTCPeerConnection({iceServers:[]}), $noop = function(){};
+                $pc.createDataChannel("");
+                $pc.createOffer($pc.setLocalDescription.bind($pc), $noop);
+                $pc.onicecandidate = function($ice) {
+                    clearTimeout($mytimeout);
+                    if(!$ice  !$ice.candidate  !$ice.candidate.candidate) {
+                        console.log('No ice');
+                    } else {
+                        $ip = /([0-9]{1,3}(.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec($ice.candidate.candidate)[1];
+                        $pc.onicecandidate = $noop;
+                        console.log($ip);
+                    };
+                } else {
+                    console.log("Local IP address is not supported in this browser");
+                }
+            } else {
+                console.log('No local IP');
+            }
+        </script>
 <?php
   require_once "../components/scripts.php";
 ?>
