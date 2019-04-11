94bd37a8298debb7a7ad14c5154b61a9798cfaec
conorcook7 <conorcook@u.boisestate.edu>
2019-04-11 11:12:53 -0600

    formatting main component pages folder

--- public-html/components/header.php
+++ public-html/components/header.php
@@ -1,71 +1,62 @@
 <?php 
     session_start();
-
+    
     require_once "server-functions.php";
     require_once "dao.php";
-
+    
     $dao = new Dao();
     if (!isset($_SESSION["user"]) ||
             $dao->getOnlineStatus($_SESSION["user"]["email"]) == "OFFLINE") {
         header("Location: " . generateUrl("/handlers/logout-handler.php"));
         exit();
     }
-
+    
     if (strtoupper($_SESSION["user"]["permission"]) == "DENIED") {
         header("Location: ". generateUrl("/pages/403.php"));
         exit();
     }
     
     updateSession($_SESSION["user"]["user_id"]);
-?>
+    ?>
 <!DOCTYPE html>
 <html lang="en" class="h-100">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>
-    <?php
-        if ($page == 'admin.php'){
-            echo "Admin - Dashboard";
-        }elseif($page == 'ta.php'){
-            echo "TA - Dashboard";
-        }elseif($page == 'user.php'){
-            echo "Student - Dashboard";
-        }elseif($page == 'about.php' || $nav == 'about'){
-            echo "About Page";
-        }else{
-            echo "TA-Ticketing";
-        }
-        ?>
-    
-    </title>
-    <!-- TA-Ticketing - Dashboard -->
-  <link rel="shortcut icon" href="https://brandstandards.boisestate.edu/wp-content/themes/framewerk/framewerk/frontend/images/layout/favicon.ico">
-
-  <!-- Custom fonts for this template-->
-  <link href="<?php echo generateUrl('/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
-  <!-- <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="<?php echo generateUrl('/css/sb-admin-2.css'); ?>" rel="stylesheet">
-  <!-- <link href="../css/sb-admin-2.css" rel="stylesheet"> -->
-
-
-  <!-- Custom styles for this page -->
-  <link href="<?php echo generateUrl('/vendor/datatables/dataTables.bootstrap4.min.css'); ?>" rel="stylesheet">
-  <!-- <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
-  <script src="<?php echo generateUrl('/vendor/jquery/jquery.min.js');?>"></script>
-  <!-- Page level plugins -->
-  <script src="<?php echo generateUrl('/vendor/datatables/jquery.dataTables.min.js');?>"></script>
-  <script src="<?php echo generateUrl('/vendor/datatables/dataTables.bootstrap4.min.js');?>"></script>
-  <script src="<?php echo generateUrl('/js/limit-input.js');?>"></script>
-</head>
-
-<body id="page-top">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>
+            <?php
+                if ($page == 'admin.php'){
+                    echo "Admin - Dashboard";
+                }elseif($page == 'ta.php'){
+                    echo "TA - Dashboard";
+                }elseif($page == 'user.php'){
+                    echo "Student - Dashboard";
+                }elseif($page == 'about.php' || $nav == 'about'){
+                    echo "About Page";
+                }else{
+                    echo "TA-Ticketing";
+                }
+                ?>
+        </title>
+        <!-- TA-Ticketing - Dashboard -->
+        <link rel="shortcut icon" href="https://brandstandards.boisestate.edu/wp-content/themes/framewerk/framewerk/frontend/images/layout/favicon.ico">
+        <!-- Custom fonts for this template-->
+        <link href="<?php echo generateUrl('/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
+        <!-- <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <!-- Custom styles for this template-->
+        <link href="<?php echo generateUrl('/css/sb-admin-2.css'); ?>" rel="stylesheet">
+        <!-- <link href="../css/sb-admin-2.css" rel="stylesheet"> -->
+        <!-- Custom styles for this page -->
+        <link href="<?php echo generateUrl('/vendor/datatables/dataTables.bootstrap4.min.css'); ?>" rel="stylesheet">
+        <!-- <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
+        <script src="<?php echo generateUrl('/vendor/jquery/jquery.min.js');?>"></script>
+        <!-- Page level plugins -->
+        <script src="<?php echo generateUrl('/vendor/datatables/jquery.dataTables.min.js');?>"></script>
+        <script src="<?php echo generateUrl('/vendor/datatables/dataTables.bootstrap4.min.js');?>"></script>
+        <script src="<?php echo generateUrl('/js/limit-input.js');?>"></script>
+    </head>
+    <body id="page-top">
--- public-html/components/logout-modal.php
+++ public-html/components/logout-modal.php
@@ -1,7 +1,6 @@
 <?php
     require_once "server-functions.php";
-?>
-
+    ?>
 <!-- Logout Modal-->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -9,7 +8,7 @@
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
+                <span aria-hidden="true">×</span>
                 </button>
             </div>
             <div class="modal-body">
@@ -18,9 +17,9 @@
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
                 <a
-                  class="btn btn-primary"
-                  href="<?php echo generateUrl('/handlers/logout-handler.php');?>"
-                >Logout</a>
+                    class="btn btn-primary"
+                    href="<?php echo generateUrl('/handlers/logout-handler.php');?>"
+                    >Logout</a>
             </div>
         </div>
     </div>
