481bf37ab97b834b3beb5ebf1dfa3062c520b98f
Hayden Phothong <haydenphothong@u.boisestate.edu>
2019-04-11 10:30:31 -0600

    adding logging to email sender

--- public-html/components/dao-traits/dao-bug-report.php
+++ public-html/components/dao-traits/dao-bug-report.php
@@ -40,6 +40,7 @@ trait DaoBugReport {
             );
             $query->setFetchMode(PDO::FETCH_ASSOC);
             $adminEmails = $query->fetchAll();
+            $logger->logDebug(basename(__FILE__) . ": " . __FUNCTION__ . ": " . "Obtained all of the admin emails");
 
             // Create the message to send
             if(!empty($adminEmails)) {
@@ -50,8 +51,10 @@ trait DaoBugReport {
                 for ($i = 1; $i < count($adminEmails); $i++) {
                     $headers .= $adminEmails[$i] . " ";
                 }
+                $logger->logDebug(basename(__FILE__) . ": " . __FUNCTION__ . ": " . "Attempting to send the email");
                 mail($to, $subject, $message, $headers);
             }
+            $logger->logDebug(basename(__FILE__) . ": " . __FUNCTION__ . ": " . "Email sent to all admins");
             
         } catch (Exception $e) {
             $this->logger->logError(basename(__FILE__) . ": " . __FUNCTION__ . ": Unable to send bug report email.");
