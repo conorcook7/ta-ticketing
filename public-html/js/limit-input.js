5fdbd63043f810749294f9bfadc07e6b9a12f158
conorcook7 <conorcook@u.boisestate.edu>
2019-04-11 10:01:03 -0600

    reverting

--- public-html/js/limit-input.js
+++ public-html/js/limit-input.js
@@ -5,18 +5,3 @@ function limitText(limitField, limitCount, limitNum) {
 		limitCount.value = limitNum - limitField.value.length;
 	}
 }
-
-$(document).ready(function() {
-	// Set the max character count for the description
-	let description = $(".description-form");
-	$(".max-char-count").html(description.attr("maxlength"));
-	description.on("change keyup mousedown paste", function() {
-	  $(".char-count").html(description.val().length);
-	});
-  
-	// Fade out the success alert.
-	setTimeout(function() {
-	  $(".alert-success").fadeOut("slow");
-	}, 5 * 1000);
-  });
-  
