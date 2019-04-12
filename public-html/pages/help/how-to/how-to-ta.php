3805c3e8aa579aee838052f2fe040bd29c6c5153
Hayden Phothong <hphothong@live.com>
2019-04-12 17:12:56 -0600

    fixing ta how-to

--- public-html/pages/help/how-to/how-to-ta.php
+++ public-html/pages/help/how-to/how-to-ta.php
@@ -14,10 +14,6 @@
             <h1 class="h2 m-0 font-weight-bold text-primary">Close a ticket</h1>
         </div>
         <div class="card-body">
-            <!-- <div class="card-title mb-4">
-                <div class="text-gray-800">
-                </div>
-                </div> -->
             <div class="row">
                 <!-- Step 1 - Locate the ticket in the table -->
                 <div class="col-xl-4 col-md-6">
@@ -29,13 +25,13 @@
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">Locate the Ticket</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
+                                    <i class="fas fa-list-alt fa-2x"></i>
                                 </div>
                             </div>
                             <p>
-                                Locate the ticket in either the <strong class="text-gray-800">My Tickets</strong> or
-                                <strong class="text-gray-800">All Open Tickets</strong> menus. These menus can be found
-                                on the left side of your screen in the navigation bar.
+                                Locate the ticket in either the <strong class="text-gray-800"><i class="fas fa-ticket-alt"></i> My Tickets</strong>
+                                or <strong class="text-gray-800"><i class="fas fa-list-ul"></i> All Open Tickets</strong> menus. These menus
+                                can be found on the left side of your screen in the navigation bar.
                             </p>
                         </div>
                     </div>
@@ -50,12 +46,12 @@
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">Close Ticket Button</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-times fa-2x text-gray-300"></i>
+                                    <span class="btn-sm btn-danger disabled">Close Ticket</span>
                                 </div>
                             </div>
                             <p>
                                 On the same row as the ticket you are ready to close, press the <strong>Close Ticket</strong>
-                                button. This will create a window for you to leave a quick description.
+                                button. This will bring you to the top of the page to leave a closing description for the ticket.
                             </p>
                         </div>
                     </div>
@@ -67,15 +63,16 @@
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2 mb-4">
                                     <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 3</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Leave a Note</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Finish Closing Button</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-align-left fa-2x text-gray-300"></i>
+                                    <span class="btn-sm btn-danger disabled">Finish Closing</span>
                                 </div>
                             </div>
                             <p>
                                 Write a note of what happened. This is so you will remember why you closed the ticket
-                                later. There is a limit to how many words you can write, so be concise!
+                                later. There is a limit to how many words you can write, so be concise! Once complete,
+                                click the <strong class="text-gray-800">Finish Closing</strong> button to close the ticket.
                             </p>
                         </div>
                     </div>
@@ -90,13 +87,9 @@
             <h1 class="h2 m-0 font-weight-bold text-primary">Reopen a ticket</h1>
         </div>
         <div class="card-body">
-            <!-- <div class="card-title mb-4">
-                <div class="text-gray-800">
-                </div>
-                </div> -->
             <div class="row">
                 <!-- Step 1 - Locate the ticket in the table -->
-                <div class="col-xl-4 col-md-6">
+                <div class="col-xl-6 col-md-6">
                     <div class="card border-left-primary h-100 floating-step">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
@@ -105,52 +98,33 @@
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">Locate the Ticket</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
+                                    <i class="fas fa-ticket-alt fa-2x"></i>
                                 </div>
                             </div>
                             <p>
-                                Locate the ticket in the table on the <strong class="text-gray-800">Closed Tickets</strong>
-                                menu. This menu can be found on the left side of your screen in the navigation bar.
+                                First, navigate to the <strong class="text-gray-800"><i class="fas fa-tasks"></i>Closed Tickets</strong>
+                                page. This menu can be found on the left side of your screen in the navigation bar. Then, locate the
+                                ticket you would like to reopen in the <strong class="text-gray-800">Closed Tickets</strong> table.
                             </p>
                         </div>
                     </div>
                 </div>
                 <!-- Step 2 - Close ticket button -->
-                <div class="col-xl-4 col-md-6">
+                <div class="col-xl-6 col-md-6">
                     <div class="card border-left-primary h-100 floating-step">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2 mb-4">
                                     <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 2</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Close Ticket Button</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Reopen Ticket Button</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-times fa-2x text-gray-300"></i>
+                                    <span class="btn-sm btn-success disabled">Reopen Ticket</span>
                                 </div>
                             </div>
                             <p>
-                                On the same row as the ticket you are ready to close, press the <strong>Close Ticket</strong>
-                                button. This will create a window for you to leave a quick description.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Step 3 - Closing description -->
-                <div class="col-xl-4 col-md-6">
-                    <div class="card border-left-primary h-100 floating-step">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2 mb-4">
-                                    <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 3</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Leave a Note</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-align-left fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                            <p>
-                                Write a note of what happened. This is so you will remember why you closed the ticket
-                                later. There is a limit to how many words you can write, so be concise!
+                                On the same row as the ticket you would like to reopen, press the <strong>Reopen Ticket</strong>
+                                button. Then, confirm that you would like to reopen the ticket.
                             </p>
                         </div>
                     </div>
