ece6f8def98d51eaaef14e860884afaf8638a4e0
hphothong <hphothong@live.com>
2019-04-12 16:01:24 -0600

    Adding admin how create a class

--- public-html/pages/help/how-to/how-to-admin.php
+++ public-html/pages/help/how-to/how-to-admin.php
@@ -15,57 +15,65 @@
         <div class="card-body">
             <div class="row">
                 <!-- Step 1 -->
-                <div class="col-xl-12 col-md-12 mb-4">
+                <div class="col-xl-6 col-md-8 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2 mb-4">
                                     <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 1</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">New Ticket Button</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Find the User</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-plus-square fa-2x text-gray-300"></i>
+                                    <i class="fas fa-list-alt fa-2x text-gray-300"></i>
                                 </div>
                             </div>
-                            <p>Create a new ticket with the "New Ticket" button on your dashboard.</p>
+                            <p>
+                                Navigate to the <strong class="text-gray-800">View Users</strong> page via the menu
+                                on the left of your screen. Locate the row of the user in the
+                                <strong class="text-gray-800">All Users</strong> table.
+                            </p>
                         </div>
                     </div>
                 </div>
                 <!-- Step 2 -->
-                <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-6 col-md-8 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2 mb-4">
                                     <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 2</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Fill in Information</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Update Button</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                    <span class="btn-sm btn-warning disabled">Update User</span>
                                 </div>
                             </div>
                             <p>
-                                Write about your issue so that the teaching assistant can start to become
-                                familiar with your problem.
+                                Click the <strong class="text-gray-800">Update User</strong> button that is on the same
+                                row as the user you would like to update. This should show you a form with information
+                                about the user.
                             </p>
                         </div>
                     </div>
                 </div>
                 <!-- Step 3 -->
-                <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-12 col-md-12 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2 mb-4">
                                     <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 3</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Wait for Assistance</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Add/Update User Button</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-clock fa-2x text-gray-300"></i>
+                                    <span class="btn-sm btn-primary disabled">Add/Update User</span>
                                 </div>
                             </div>
-                            <p>Continue working on other parts of the homework, while a teaching assistant
-                                becomes available.
+                            <p>
+                                Once you have updated the information about the user, you may then click the
+                                <strong class="text-gray-800">Add/Update User Button</strong> at the bottom.
+                                This will check if the user email exists, upon finding a valid user it will then
+                                update his/her information.
                             </p>
                         </div>
                     </div>
@@ -73,65 +81,202 @@
             </div>
         </div>
     </div>
-    <!-- Update a  -->
+
+    <!-- Close a Bug Report  -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h1 class="h2 m-0 font-weight-bold text-primary">Update a class</h1>
+            <h1 class="h2 m-0 font-weight-bold text-primary">Resolve a Bug Report</h1>
         </div>
         <div class="card-body">
             <div class="row">
                 <!-- Step 1 -->
-                <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-5 col-md-5 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2 mb-4">
                                     <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 1</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">New Ticket Button</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Locate the Bug Report</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-plus-square fa-2x text-gray-300"></i>
+                                    <i class="fas fa-list-alt fa-2x text-gray-300"></i>
                                 </div>
                             </div>
-                            <p>Create a new ticket with the "New Ticket" button on your dashboard.</p>
+                            <p>
+                                First, navigate to the <strong class="text-gray-800"><i class="fas fa-fw fa-bug"></i> Bug Reports</strong>
+                                menu on the left of your screen. Locate the bug report in the 
+                                <strong class="text-gray-800">All Bug Reports</strong> table.
+                            </p>
                         </div>
                     </div>
                 </div>
                 <!-- Step 2 -->
-                <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-7 col-md-7 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2 mb-4">
                                     <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 2</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Fill in Information</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Resolve Button</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                    <span class="btn-sm btn-success disabled">Resolve</span>
                                 </div>
                             </div>
                             <p>
-                                Write about your issue so that the teaching assistant can start to become
-                                familiar with your problem.
+                                Now that you have located the bug report that you would like to resolve, you are able
+                                to click the <strong class="text-gray-800">Resolve</strong> button that corresponds to
+                                the same row as the bug report you would like to close.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Create a class -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h1 class="h2 m-0 font-weight-bold text-primary">Create a Class</h1>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <!-- Step 1 -->
+                <div class="col-xl-6 col-md-8 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2 mb-4">
+                                    <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 1</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Create a Class Page</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-chalkboard-teacher"></i> Create/Delete Classes
+                                </div>
+                            </div>
+                            <p>
+                                Navigate to the <strong class="text-gray-800">View Users</strong> page via the menu
+                                on the left of your screen. Locate the row of the user in the
+                                <strong class="text-gray-800">All Users</strong> table.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Step 2 -->
+                <div class="col-xl-6 col-md-8 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2 mb-4">
+                                    <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 2</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Update Button</div>
+                                </div>
+                                <div class="col-auto">
+                                    <span class="btn-sm btn-warning disabled">Update User</span>
+                                </div>
+                            </div>
+                            <p>
+                                Click the <strong class="text-gray-800">Update User</strong> button that is on the same
+                                row as the user you would like to update. This should show you a form with information
+                                about the user.
                             </p>
                         </div>
                     </div>
                 </div>
                 <!-- Step 3 -->
-                <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-12 col-md-12 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2 mb-4">
                                     <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 3</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Wait for Assistance</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Add/Update User Button</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-clock fa-2x text-gray-300"></i>
+                                    <span class="btn-sm btn-primary disabled">Add/Update User</span>
                                 </div>
                             </div>
-                            <p>Continue working on other parts of the homework, while a teaching assistant
-                                becomes available.
+                            <p>
+                                Once you have updated the information about the user, you may then click the
+                                <strong class="text-gray-800">Add/Update User Button</strong> at the bottom.
+                                This will check if the user email exists, upon finding a valid user it will then
+                                update his/her information.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Delete a Class -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h1 class="h2 m-0 font-weight-bold text-primary">Delete a Class</h1>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <!-- Step 1 -->
+                <div class="col-xl-6 col-md-8 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2 mb-4">
+                                    <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 1</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Find the User</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-list-alt fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                            <p>
+                                Navigate to the <strong class="text-gray-800">View Users</strong> page via the menu
+                                on the left of your screen. Locate the row of the user in the
+                                <strong class="text-gray-800">All Users</strong> table.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Step 2 -->
+                <div class="col-xl-6 col-md-8 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2 mb-4">
+                                    <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 2</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Update Button</div>
+                                </div>
+                                <div class="col-auto">
+                                    <span class="btn-sm btn-warning disabled">Update User</span>
+                                </div>
+                            </div>
+                            <p>
+                                Click the <strong class="text-gray-800">Update User</strong> button that is on the same
+                                row as the user you would like to update. This should show you a form with information
+                                about the user.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Step 3 -->
+                <div class="col-xl-12 col-md-12 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2 mb-4">
+                                    <div class="h5 font-weight-bold text-primary text-uppercase mb-1">Step 3</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Add/Update User Button</div>
+                                </div>
+                                <div class="col-auto">
+                                    <span class="btn-sm btn-primary disabled">Add/Update User</span>
+                                </div>
+                            </div>
+                            <p>
+                                Once you have updated the information about the user, you may then click the
+                                <strong class="text-gray-800">Add/Update User Button</strong> at the bottom.
+                                This will check if the user email exists, upon finding a valid user it will then
+                                update his/her information.
                             </p>
                         </div>
                     </div>
