2fe54795257b0eaa9cbd91e607d1d3f7fd94234f
Hayden Phothong <hphothong@live.com>
2019-04-13 13:06:28 -0600

    Adding fluid container for resizing

--- public-html/pages/help/bug-report.php
+++ public-html/pages/help/bug-report.php
@@ -25,60 +25,62 @@
         <div id="content">
 
             <!-- Main card -->
-            <div class="d-flex justify-content-center align-items-center mt-4 h-75">
-                <div class="card shadow w-50">
-                    <div class="card-header">
-                        <span class="h3 mb-0 text-gray-800">Report a Problem</span>
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text"><i class="fas fa-exclamation-circle"></i> Reporting an issue will notify all of the website administrators via email.</p>
-                        <?php if (isset($_SESSION["success"])){ ?>
-                        <div class="alert alert-success">
-                            <strong>Success!</strong> <?php echo $_SESSION["success"]; ?>
+            <div class="container-fluid">
+                <div class="d-flex justify-content-center align-items-center mt-4 h-75">
+                    <div class="card shadow w-50">
+                        <div class="card-header">
+                            <span class="h3 mb-0 text-gray-800">Report a Problem</span>
                         </div>
-                        <?php } elseif (isset($_SESSION["failure"])) { ?>
-                        <div class="alert alert-danger">
-                            <strong>Failure!</strong> <?php echo $_SESSION["failure"]; ?>
-                        </div>
-                        <?php }
-                            unset($_SESSION["failure"]);
-                            unset($_SESSION["success"]);
-                            ?>
-                        <form id="bug-form" method="POST" action="../../handlers/bug-report-handler.php">
-                            <label for="title">Issue</label>
-                            <div class="form-group w-50">
-                                <input
-                                    type="text"
-                                    id="title"
-                                    name="title"
-                                    class="form-control"
-                                    placeholder="Unable to create ticket..."
-                                    value="<?php echo isset($_SESSION["presets"]["title"]) ? $_SESSION["presets"]["title"] : ""; ?>"
-                                    required="true"
-                                    maxlength=45
-                                    />
+                        <div class="card-body">
+                            <p class="card-text"><i class="fas fa-exclamation-circle"></i> Reporting an issue will notify all of the website administrators via email.</p>
+                            <?php if (isset($_SESSION["success"])){ ?>
+                            <div class="alert alert-success">
+                                <strong>Success!</strong> <?php echo $_SESSION["success"]; ?>
                             </div>
-                            <label for="description">Description</label>
-                            <div class="form-group">
-                                <textarea 
-                                    id="description"
-                                    name="description"
-                                    class="form-control"
-                                    rows=10
-                                    maxlength=4000
-                                    style="resize: none"
-                                    placeholder="I was unable to create a ticket because..."
-                                    ><?php echo isset($_SESSION["presets"]["description"]) ? $_SESSION["presets"]["description"] : ""; ?></textarea>
-                                <div class="text-right">
-                                    <span id="char-count">0</span>
-                                    <span>/</span>
-                                    <span id="max-char-count">4000</span>
+                            <?php } elseif (isset($_SESSION["failure"])) { ?>
+                            <div class="alert alert-danger">
+                                <strong>Failure!</strong> <?php echo $_SESSION["failure"]; ?>
+                            </div>
+                            <?php }
+                                unset($_SESSION["failure"]);
+                                unset($_SESSION["success"]);
+                                ?>
+                            <form id="bug-form" method="POST" action="../../handlers/bug-report-handler.php">
+                                <label for="title">Issue</label>
+                                <div class="form-group w-50">
+                                    <input
+                                        type="text"
+                                        id="title"
+                                        name="title"
+                                        class="form-control"
+                                        placeholder="Unable to create ticket..."
+                                        value="<?php echo isset($_SESSION["presets"]["title"]) ? $_SESSION["presets"]["title"] : ""; ?>"
+                                        required="true"
+                                        maxlength=45
+                                        />
                                 </div>
-                            </div>
-                            <div class="d-flex form-group justify-content-end">
-                                <button type="submit" class="btn btn-primary">Report Issue</button>
-                            </div>
-                        </form>
+                                <label for="description">Description</label>
+                                <div class="form-group">
+                                    <textarea 
+                                        id="description"
+                                        name="description"
+                                        class="form-control"
+                                        rows=10
+                                        maxlength=4000
+                                        style="resize: none"
+                                        placeholder="I was unable to create a ticket because..."
+                                        ><?php echo isset($_SESSION["presets"]["description"]) ? $_SESSION["presets"]["description"] : ""; ?></textarea>
+                                    <div class="text-right">
+                                        <span id="char-count">0</span>
+                                        <span>/</span>
+                                        <span id="max-char-count">4000</span>
+                                    </div>
+                                </div>
+                                <div class="d-flex form-group justify-content-end">
+                                    <button type="submit" class="btn btn-primary">Report Issue</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
