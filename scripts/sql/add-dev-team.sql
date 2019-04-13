6692f1ae74e39d46271a7d830e584271796be4a9
Hayden Phothong <hphothong@live.com>
2019-04-13 00:49:44 -0600

    fixing ta insert user_id

--- scripts/sql/add-dev-team.sql
+++ scripts/sql/add-dev-team.sql
@@ -1,7 +1,7 @@
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (2, 1, 'michaelsanchez563@u.boisestate.edu', 'Michael', 'Sanchez');
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (3, 2, 'conorcook@u.boisestate.edu', 'Conor', 'Cook');
 INSERT INTO Available_Courses (course_name, course_number, course_description) VALUES ('Computer Science I', 'CS121', 'Introduction to Java Programming');
-INSERT INTO Teaching_Assistants (user_id, available_course_id, start_time_past_midnight, end_time_past_midnight) VALUES (403, 1, '08:00:00', '16:00:00');
+INSERT INTO Teaching_Assistants (user_id, available_course_id, start_time_past_midnight, end_time_past_midnight) VALUES (3, 1, '08:00:00', '16:00:00');
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (4, 3, 'malikherring@u.boisestate.edu', 'Malik', 'Herring');
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (5, 3, 'haydenphothong@u.boisestate.edu', 'Hayden', 'Phothong');
 INSERT INTO Users (permission_id, email, first_name, last_name) VALUES (3, 'shanepanter@boisestate.edu', 'Shane', 'Panter');
