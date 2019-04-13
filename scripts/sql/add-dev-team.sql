ccb13ed1e8873a6d688fd48f328a3796cda3dbd7
Hayden Phothong <hphothong@live.com>
2019-04-13 00:48:09 -0600

    Adding CS121 to dev team script

--- scripts/sql/add-dev-team.sql
+++ scripts/sql/add-dev-team.sql
@@ -1,5 +1,6 @@
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (2, 1, 'michaelsanchez563@u.boisestate.edu', 'Michael', 'Sanchez');
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (3, 2, 'conorcook@u.boisestate.edu', 'Conor', 'Cook');
+INSERT INTO Available_Courses (course_name, course_number, course_description) VALUES ('Computer Science I', 'CS121', 'Introduction to Java Programming');
 INSERT INTO Teaching_Assistants (user_id, available_course_id, start_time_past_midnight, end_time_past_midnight) VALUES (403, 1, '08:00:00', '16:00:00');
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (4, 3, 'malikherring@u.boisestate.edu', 'Malik', 'Herring');
 INSERT INTO Users (user_id, permission_id, email, first_name, last_name) VALUES (5, 3, 'haydenphothong@u.boisestate.edu', 'Hayden', 'Phothong');
