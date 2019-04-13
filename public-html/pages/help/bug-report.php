e7841fa8acd13f9483378af816ab392c8964810d
Hayden Phothong <hphothong@live.com>
2019-04-13 01:13:55 -0600

    Possible A/B Testing of forms

--- public-html/pages/help/bug-report.php
+++ public-html/pages/help/bug-report.php
@@ -23,57 +23,59 @@
         
         <!-- Main Content -->
         <div id="content">
-            <div class="container-fluid px-4">
-                <div class="d-sm-flex align-items-center justify-content-between mb-4 border-bottom">
-                    <span class="h3 mb-0 text-gray-800">Report a Problem</span>
-                </div>
-                <div>
-                    <?php if (isset($_SESSION["success"])){ ?>
-                    <div class="alert alert-success">
-                        <strong>Success!</strong> <?php echo $_SESSION["success"]; ?>
-                    </div>
-                    <?php } elseif (isset($_SESSION["failure"])) { ?>
-                    <div class="alert alert-danger">
-                        <strong>Failure!</strong> <?php echo $_SESSION["failure"]; ?>
-                    </div>
-                    <?php }
-                        unset($_SESSION["failure"]);
-                        unset($_SESSION["success"]);
-                        ?>
-                    <form id="bug-form" method="POST" action="../../handlers/bug-report-handler.php">
-                        <label for="title">Issue</label>
-                        <div class="form-group w-25">
-                            <input
-                                type="text"
-                                id="title"
-                                name="title"
-                                class="form-control"
-                                placeholder="Unable to create ticket..."
-                                value="<?php echo isset($_SESSION["presets"]["title"]) ? $_SESSION["presets"]["title"] : ""; ?>"
-                                required="true"
-                                maxlength=45
-                                />
+            <div class="container-fluid px-4 h-100">
+                <div class="d-flex justify-content-center align-items-center h-75">
+                    <div class="w-50">
+                        <div class="mb-4 border-bottom">
+                            <span class="h3 mb-0 text-gray-800">Report a Problem</span>
                         </div>
-                        <label for="description">Description</label>
-                        <div class="form-group">
-                            <textarea 
-                                id="description"
-                                name="description"
-                                class="form-control"
-                                rows=5
-                                maxlength=4000
-                                placeholder="I was unable to create a ticket because..."
-                                ><?php echo isset($_SESSION["presets"]["description"]) ? $_SESSION["presets"]["description"] : ""; ?></textarea>
-                            <div class="text-right">
-                                <span id="char-count">0</span>
-                                <span>/</span>
-                                <span id="max-char-count">1000</span>
+                        <?php if (isset($_SESSION["success"])){ ?>
+                        <div class="alert alert-success">
+                            <strong>Success!</strong> <?php echo $_SESSION["success"]; ?>
+                        </div>
+                        <?php } elseif (isset($_SESSION["failure"])) { ?>
+                        <div class="alert alert-danger">
+                            <strong>Failure!</strong> <?php echo $_SESSION["failure"]; ?>
+                        </div>
+                        <?php }
+                            unset($_SESSION["failure"]);
+                            unset($_SESSION["success"]);
+                            ?>
+                        <form id="bug-form" method="POST" action="../../handlers/bug-report-handler.php">
+                            <label for="title">Issue</label>
+                            <div class="form-group w-50">
+                                <input
+                                    type="text"
+                                    id="title"
+                                    name="title"
+                                    class="form-control"
+                                    placeholder="Unable to create ticket..."
+                                    value="<?php echo isset($_SESSION["presets"]["title"]) ? $_SESSION["presets"]["title"] : ""; ?>"
+                                    required="true"
+                                    maxlength=45
+                                    />
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">Report Issue</button>
-                        </div>
-                    </form>
+                            <label for="description">Description</label>
+                            <div class="form-group">
+                                <textarea 
+                                    id="description"
+                                    name="description"
+                                    class="form-control"
+                                    rows=5
+                                    maxlength=4000
+                                    placeholder="I was unable to create a ticket because..."
+                                    ><?php echo isset($_SESSION["presets"]["description"]) ? $_SESSION["presets"]["description"] : ""; ?></textarea>
+                                <div class="text-right">
+                                    <span id="char-count">0</span>
+                                    <span>/</span>
+                                    <span id="max-char-count">1000</span>
+                                </div>
+                            </div>
+                            <div class="d-flex form-group justify-content-end">
+                                <button type="submit" class="btn btn-primary">Report Issue</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
