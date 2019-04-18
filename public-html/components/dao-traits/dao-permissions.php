682262a502c6b94a523bb9d4d9229b79acd531e1
Hayden Phothong <hphothong@live.com>
2019-04-17 20:53:08 -0600

    get permission by id

--- public-html/components/dao-traits/dao-permissions.php
+++ public-html/components/dao-traits/dao-permissions.php
@@ -28,6 +28,30 @@ trait DaoPermissions {
     }
 
     /**
+     * Get the permission name.
+     * 
+     * @param $permissionId - The permission id
+     * @return $permissionName - All caps version of the permission name
+     */
+    public function getPermissionById($permissionId) {
+        try {
+            $conn = $this->getConnection();
+            $query = $conn->prepare("SELECT permission_name FROM Permissions WHERE permission_id = :permissionId");
+            $query->bindParam(":permissionId", $permissionId);
+            $query->setFetchMode(PDO::FETCH_ASSOC);
+            $query->execute();
+            $permissionName = $query->fetch()["permission_name"];
+            $permissionName = strtoupper($permissionName);
+            $this->logger->logDebug(basename(__FILE__) . ":" . __FUNCTION__ . "(): Obtained permission levels");
+            return $permissionName;
+        } catch (Exception $e) {
+            $this->logger->logError(basename(__FILE__) . ":" . __FUNCTION__ . "(): Unable to get permission levels");
+            $this->logger->logError(basename(__FILE__) . ":" . __FUNCTION__ . "(): " . $e->getMessage());
+            return NULL;
+        }
+    }
+
+    /**
      * Attempts to create a permission level. The method will fail if the
      * permisson name already exists in the database.
      * 
